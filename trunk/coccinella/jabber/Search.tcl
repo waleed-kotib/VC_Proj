80653288f0637d8c641d0d45e31696a75233840a
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2003-11-14 09:56:02 +0000

    * Broken out from Jabber.tcl.

---
+++ trunk/coccinella/jabber/Search.tcl
@@ -0,0 +1,328 @@
+#  Search.tcl ---
+#  
+#      This file is part of the whiteboard application. 
+#      It implements search UI parts for jabber.
+#      
+#  Copyright (c) 2001-2003  Mats Bengtsson
+#  
+# $Id: Search.tcl,v 1.1 2003-11-14 09:55:23 matben Exp $
+
+package provide Search 1.0
+
+
+namespace eval ::Jabber::Search:: {
+
+    # Wait for this variable to be set.
+    variable finished  
+}
+
+# Jabber::Search::Build --
+#
+#       Initiates the process of searching a service.
+#       
+# Arguments:
+#       w           toplevel widget
+#       args   -server, -autoget 0/1
+#       
+# Results:
+#       .
+     
+proc ::Jabber::Search::Build {w args} {
+    global  this sysFont prefs
+
+    variable wtop
+    variable wbox
+    variable wbtsearch
+    variable wbtget
+    variable wcomboserver
+    variable wtb
+    variable wxsc
+    variable wysc
+    variable woob
+    variable server
+    variable wsearrows
+    variable stattxt
+    upvar ::Jabber::jstate jstate
+    
+    if {[winfo exists $w]} {
+	return
+    }
+    array set argsArr $args
+    set finished -1
+    
+    toplevel $w
+    if {[string match "mac*" $this(platform)]} {
+	eval $::macWindowStyle $w documentProc
+	::UI::MacUseMainMenu $w
+    } else {
+
+    }
+    wm title $w [::msgcat::mc {Search Service}]
+    set wtop $w
+    
+    # Global frame.
+    pack [frame $w.frall -borderwidth 1 -relief raised]   \
+      -fill both -expand 1 -ipadx 6 -ipady 4
+    
+    # Left half.
+    set wleft $w.frall.fl
+    pack [frame $wleft] -side left -fill y
+    
+    # Right half.
+    set wright $w.frall.fr
+    pack [frame $wright] -side right -expand 1 -fill both
+    
+    message $wleft.msg -width 200 -font $sysFont(s)  \
+      -text [::msgcat::mc jasearch] -anchor w
+    pack $wleft.msg -side top -fill x -anchor w -padx 4 -pady 2
+    set frtop $wleft.top
+    pack [frame $frtop] -side top -fill x -anchor w -padx 4 -pady 2
+    label $frtop.lserv -text "[::msgcat::mc {Search Service}]:" -font $sysFont(sb)
+    
+    # Button part.
+    set frbot [frame $wleft.frbot -borderwidth 0]
+    set wsearrows $frbot.arr
+    set wbtsearch $frbot.btenter
+    pack [button $wbtsearch -text [::msgcat::mc Search] -width 8 -state disabled \
+      -command [namespace current]::DoSearch]  \
+      -side right -padx 5 -pady 2
+    pack [button $frbot.btcancel -text [::msgcat::mc Cancel] -width 8  \
+      -command "destroy $w"]  \
+      -side right -padx 5 -pady 2
+    pack [::chasearrows::chasearrows $wsearrows -background gray87 -size 16] \
+      -side left -padx 5 -pady 2
+    pack $frbot -side bottom -fill x -padx 8 -pady 6
+    
+    # OOB alternative.
+    set woob [frame $wleft.foob]
+    pack $woob -side bottom -fill x -padx 8 -pady 0
+    
+    # Get all (browsed) services that support search.
+    set searchServ [$jstate(jlib) service getjidsfor "search"]
+    set wcomboserver $frtop.eserv
+    ::combobox::combobox $wcomboserver -width 20 -font $sysFont(s)   \
+      -textvariable [namespace current]::server -editable 0
+    eval {$frtop.eserv list insert end} $searchServ
+    
+    # Find the default search server.
+    if {[llength $searchServ]} {
+	set server [lindex $searchServ 0]
+    }
+    if {[info exists argsArr(-server)]} {
+	set server $argsArr(-server)
+	$wcomboserver configure -state disabled
+    }
+    
+    # Get button.
+    set wbtget $frtop.btget
+    button $wbtget -text [::msgcat::mc Get] -width 6 -default active \
+      -command [list ::Jabber::Search::Get]
+
+    grid $frtop.lserv -sticky w
+    grid $wcomboserver -row 1 -column 0 -sticky ew
+    grid $wbtget -row 1 -column 1 -sticky e -padx 2
+
+    # This part must be built dynamically from the 'get' xml data.
+    # May be different for each conference server.
+    set wfr $wleft.frlab
+    set wcont [LabeledFrame2 $wfr [::msgcat::mc {Search Specifications}]]
+    pack $wfr -side top -fill both -padx 2 -pady 2
+
+    set wbox [frame $wcont.box]
+    pack $wbox -side left -fill both -padx 4 -pady 4 -expand 1
+    pack [label $wbox.la -textvariable "[namespace current]::stattxt"]  \
+      -padx 0 -pady 10 -side left
+    set stattxt "-- [::msgcat::mc jasearchwait] --"
+    
+    # The Search result tablelist widget.
+    set frsearch $wright.se
+    pack [frame $frsearch -borderwidth 1 -relief sunken] -side top -fill both \
+      -expand 1 -padx 4 -pady 4
+    set wtb $frsearch.tb
+    set wxsc $frsearch.xsc
+    set wysc $frsearch.ysc
+    tablelist::tablelist $wtb \
+      -columns [list 60 [::msgcat::mc {Search results}]]  \
+      -font $sysFont(s) -labelfont $sysFont(s) -background white  \
+      -xscrollcommand [list $wxsc set] -yscrollcommand [list $wysc set]  \
+      -labelbackground #cecece -stripebackground #dedeff -width 60 -height 20
+    #-labelcommand "[namespace current]::LabelCommand"  \
+    
+    scrollbar $wysc -orient vertical -command [list $wtb yview]
+    scrollbar $wxsc -orient horizontal -command [list $wtb xview]
+    grid $wtb $wysc -sticky news
+    grid $wxsc -sticky ew -column 0 -row 1
+    grid rowconfigure $frsearch 0 -weight 1
+    grid columnconfigure $frsearch 0 -weight 1
+    
+    wm minsize $w 400 320
+	    
+    # If only a single search service, or if specified as argument.
+    if {([llength $searchServ] == 1) ||  \
+      [info exists argsArr(-autoget)] && $argsArr(-autoget)} {
+	::Jabber::Search::Get
+    }
+}
+
+proc ::Jabber::Search::Get { } {    
+    variable server
+    variable wsearrows
+    variable wcomboserver
+    variable wbtget
+    variable wtb
+    variable stattxt
+    upvar ::Jabber::jstate jstate
+    
+    # Verify.
+    if {[string length $server] == 0} {
+	tk_messageBox -type ok -icon error  \
+	  -message [::msgcat::mc jamessregnoserver]
+	return
+    }	
+    $wcomboserver configure -state disabled
+    $wbtget configure -state disabled
+    set stattxt "-- [::msgcat::mc jawaitserver] --"
+    
+    # Send get register.
+    $jstate(jlib) search_get $server ::Jabber::Search::GetCB    
+    $wsearrows start
+    
+    $wtb configure -columns [list 60 [::msgcat::mc {Search results}]]
+    $wtb delete 0 end
+}
+
+# Jabber::Search::GetCB --
+#
+#       This is the 'get' iq callback.
+#       It should be possible to receive multiple callbacks for a single
+#       search, but this is untested.
+
+proc ::Jabber::Search::GetCB {jlibName type subiq} {
+    global  sysFont
+    
+    variable wtop
+    variable wbox
+    variable wtb
+    variable wxsc
+    variable wysc
+    variable woob
+    variable wsearrows
+    variable wbtsearch
+    variable wbtget
+    upvar ::Jabber::jstate jstate
+    upvar ::Jabber::jprefs jprefs
+    
+    ::Jabber::Debug 2 "::Jabber::Search::GetCB type=$type, subiq='$subiq'"
+    
+    if {![winfo exists $wtop]} {
+	return
+    }
+    $wsearrows stop
+    
+    if {$type == "error"} {
+	tk_messageBox -type ok -icon error  \
+	  -message [FormatTextForMessageBox \
+	  [::msgcat::mc jamesserrsearch [lindex $subiq 0] [lindex $subiq 1]]]
+	return
+    }
+    catch {destroy $wbox}
+    catch {destroy $woob.oob}
+    set subiqChildList [wrapper::getchildren $subiq]
+    
+    # We must figure out if we have an oob thing.
+    set hasOOBForm 0
+    foreach c $subiqChildList {
+	if {[string equal [lindex $c 0] "x"]} {
+	    array set cattrArr [lindex $c 1]
+	    if {[info exists cattrArr(xmlns)] &&  \
+	      [string equal $cattrArr(xmlns) "jabber:x:oob"]} {
+		set hasOOBForm 1
+		set xmlOOBElem $c
+	    }
+	}
+    }
+	
+    # Build form dynamically from XML.
+    ::Jabber::Forms::Build $wbox $subiqChildList -template "search" -width 160
+    pack $wbox -side left -padx 2 -pady 10
+    if {$hasOOBForm} {
+	set woobtxt [::Jabber::OOB::BuildText ${woob}.oob $xmlOOBElem]
+	pack $woobtxt -side top -fill x
+    }
+    $wbtsearch configure -state normal -default active
+    $wbtget configure -state normal -default disabled   
+}
+
+proc ::Jabber::Search::DoSearch { } {    
+    variable server
+    variable wsearrows
+    variable wbox
+    variable wtb
+    upvar ::Jabber::jstate jstate
+    upvar ::Jabber::jprefs jprefs
+    
+    $wsearrows start
+    $wtb delete 0 end
+
+    # Returns the hierarchical xml list starting with the <x> element.
+    set subelements [::Jabber::Forms::GetXML $wbox]    
+    $jstate(jlib) search_set $server  \
+      [list [namespace current]::ResultCallback $server] -subtags $subelements
+}
+
+# Jabber::Search::ResultCallback --
+#
+#       This is the 'result' and 'set' iq callback We may get a number of server
+#       pushing 'set' elements, finilized by the 'result' element.
+#       
+#       Update: the situation with jabber:x:data seems unclear here.
+#       
+# Arguments:
+#       server:
+#       type:       "ok", "error", or "set"
+#       subiq:
+
+proc ::Jabber::Search::ResultCallback {server type subiq} {   
+    variable wtop
+    variable wtb
+    variable wbox
+    variable wsearrows
+    upvar ::Jabber::jstate jstate
+    upvar ::Jabber::jprefs jprefs
+    
+    ::Jabber::Debug 2 "::Jabber::Search::ResultCallback server=$server, type=$type, \
+      subiq='$subiq'"
+    
+    if {![winfo exists $wtop]} {
+	return
+    }
+    $wsearrows stop
+    if {[string equal $type "error"]} {
+	foreach {ecode emsg} [lrange $subiq 0 1] break
+	if {$ecode == "406"} {
+	    set msg "There was an invalid field. Please correct it: $emsg"
+	} else {
+	    set msg "Failed searching service. Error code $ecode with message: $emsg"
+	}
+	tk_messageBox -type ok -icon error -message [FormatTextForMessageBox $msg]
+	return
+    } elseif {[string equal $type "ok"]} {
+	
+	# This returns the search result and sets the reported stuff.
+	set columnSpec {}
+	set resultList [::Jabber::Forms::ResultList $wbox $subiq]
+	foreach {var label} [::Jabber::Forms::GetReported $wbox] {
+	    lappend columnSpec 0 $label	    
+	}
+	$wtb configure -columns $columnSpec
+	if {[llength $resultList] == 0} {
+	    $wtb insert end {{No matches found}}
+	} else {
+	    foreach row $resultList {
+		$wtb insert end $row
+	    }
+	}
+    }
+}
+
+#-------------------------------------------------------------------------------
---
+++ trunk/coccinella/jabber/Subscribe.tcl
@@ -0,0 +1,248 @@
+#  Subscribe.tcl ---
+#  
+#      This file is part of the whiteboard application. 
+#      It implements subscription parts.
+#      
+#  Copyright (c) 2001-2003  Mats Bengtsson
+#  
+# $Id: Subscribe.tcl,v 1.1 2003-11-14 09:56:02 matben Exp $
+
+package provide Subscribe 1.0
+
+namespace eval ::Jabber::Subscribe:: {
+
+    # Store everything in 'locals($uid, ... )'.
+    variable locals   
+    variable uid 0
+}
+
+# Jabber::Subscribe::Subscribe --
+#
+#       Ask for user response on a subscribe presence element.
+#
+# Arguments:
+#       wbase  the toplevel window's base path.
+#       jid    the jid we receive a 'subscribe' presence element from.
+#       args   ?-key value ...? look for any '-status' only.
+#       
+# Results:
+#       "deny" or "accept".
+
+proc ::Jabber::Subscribe::Subscribe {wbase jid args} {
+    global  this sysFont prefs wDlgs
+    
+    variable locals   
+    variable uid
+    upvar ::Jabber::jstate jstate
+    upvar ::Jabber::jprefs jprefs
+    
+    ::Jabber::Debug 2 "::Jabber::Subscribe::Subscribe jid=$jid"
+
+    incr uid
+    set w ${wbase}${uid}
+    set locals($uid,finished) -1
+    set locals($uid,wtop) $w
+    set locals($uid,jid) $jid
+    array set argsArr $args
+    
+    toplevel $w
+    if {[string match "mac*" $this(platform)]} {
+	eval $::macWindowStyle $w documentProc
+    } else {
+
+    }
+    wm title $w [::msgcat::mc Subscribe]
+    
+    # Find our present groups.
+    set allGroups [$jstate(roster) getgroups]
+	
+    # This gets a list '-name ... -groups ...' etc. from our roster.
+    # Note! -groups PLURAL!
+    array set itemAttrArr {-name {} -groups {} -subscription none}
+    array set itemAttrArr [$jstate(roster) getrosteritem $jid]
+    
+    # Textvariables for entry and combobox.
+    set locals($uid,name) $itemAttrArr(-name)
+    if {[llength $itemAttrArr(-groups)] > 0} {
+	set locals($uid,group) [lindex $itemAttrArr(-groups) 0]
+    }
+    
+    # Figure out if we shall send a 'subscribe' presence to this user.
+    set maySendSubscribe 1
+    if {$itemAttrArr(-subscription) == "to" ||  \
+      $itemAttrArr(-subscription) == "both"} {
+	set maySendSubscribe 0
+    }
+
+    # Global frame.
+    pack [frame $w.frall -borderwidth 1 -relief raised]  \
+      -fill both -expand 1 -ipadx 4
+    
+    label $w.frall.head -text [::msgcat::mc Subscribe] -font $sysFont(l)  \
+      -anchor w -padx 10 -pady 4 -bg #cecece
+    pack $w.frall.head -side top -fill both -expand 1
+    message $w.frall.msg -width 260 -font $sysFont(s)  \
+      -text [::msgcat::mc jasubwant $jid]
+    pack $w.frall.msg -side top -fill both -expand 1
+    
+    # Any -status attribute?
+    if {[info exists argsArr(-status)] && [string length $argsArr(-status)]} {
+	set txt "Message: \"$argsArr(-status)\""
+	label $w.frall.status -wraplength 260 -text $txt
+	pack $w.frall.status -side top -anchor w -padx 10
+    }
+	
+    # Some action buttons.
+    set frmid [frame $w.frall.frmid -borderwidth 0]
+    label $frmid.lvcard -text "[::msgcat::mc jasubgetvcard]:" -font $sysFont(sb) \
+      -anchor e
+    button $frmid.bvcard -text "[::msgcat::mc {Get vCard}]..."   \
+      -command [list ::VCard::Fetch .kass other $jid]
+    label $frmid.lmsg -text [::msgcat::mc jasubsndmsg]   \
+      -font $sysFont(sb) -anchor e
+    button $frmid.bmsg -text "[::msgcat::mc Send]..."    \
+      -command [list ::Jabber::Subscribe::SendMsg $uid]
+    grid $frmid.lvcard -column 0 -row 0 -sticky e -padx 6 -pady 2
+    grid $frmid.bvcard -column 1 -row 0 -sticky ew -padx 6 -pady 2
+    grid $frmid.lmsg -column 0 -row 1 -sticky e -padx 6 -pady 2
+    grid $frmid.bmsg -column 1 -row 1 -sticky ew -padx 6 -pady 2
+    pack $frmid -side top -fill both -expand 1
+
+    # The option part.
+    set locals($uid,allow) 1
+    set locals($uid,add) $jprefs(defSubscribe)
+    set fropt $w.frall.fropt
+    set frcont [LabeledFrame2 $fropt {Options}]
+    pack $fropt -side top -fill both -ipadx 10 -ipady 6
+    checkbutton $frcont.pres -text "  [::msgcat::mc jasuballow $jid]" \
+      -variable [namespace current]::locals($uid,allow)
+    
+    checkbutton $frcont.add -text "  [::msgcat::mc jasubadd $jid]" \
+      -variable [namespace current]::locals($uid,add)
+    pack $frcont.pres $frcont.add -side top -anchor w -padx 10 -pady 4
+    set frsub [frame $frcont.frsub]
+    pack $frsub -expand 1 -fill x -side top
+    label $frsub.lnick -text "[::msgcat::mc {Nick name}]:" -font $sysFont(sb) \
+      -anchor e
+    entry $frsub.enick -width 26  \
+      -textvariable [namespace current]::locals($uid,name)
+    label $frsub.lgroup -text "[::msgcat::mc Group]:" -font $sysFont(sb) -anchor e
+    
+    ::combobox::combobox $frsub.egroup -font $sysFont(s) -width 18  \
+      -textvariable [namespace current]::locals($uid,group)
+    eval {$frsub.egroup list insert end} "None $allGroups"
+    
+    grid $frsub.lnick -column 0 -row 0 -sticky e
+    grid $frsub.enick -column 1 -row 0 -sticky ew
+    grid $frsub.lgroup -column 0 -row 1 -sticky e
+    grid $frsub.egroup -column 1 -row 1 -sticky w
+    
+    # If we may NOT send a 'subscribe' presence to this user.
+    if {!$maySendSubscribe} {
+	set locals($uid,add) 0
+	$frcont.add configure -state disabled
+	$frsub.enick configure -state disabled
+	$frsub.egroup configure -state disabled
+    }
+    
+    # Button part.
+    set frbot [frame $w.frall.frbot -borderwidth 0]
+    pack [button $frbot.btconn -text [::msgcat::mc Accept] -width 8 -default active \
+      -command [list [namespace current]::Doit $uid]]  \
+      -side right -padx 5 -pady 5
+    pack [button $frbot.btcancel -text [::msgcat::mc Deny] -width 8   \
+      -command [list [namespace current]::Cancel $uid]]  \
+      -side right -padx 5 -pady 5
+    pack $frbot -side top -fill both -expand 1 -padx 8 -pady 6
+    
+    wm resizable $w 0 0
+    bind $w <Return> "$frbot.btconn invoke"
+    focus $w
+    
+    # Wait here for a button press and window to be destroyed.
+    tkwait window $w
+    
+    # Cleanup.
+    set finito $locals($uid,finished)
+    foreach key [array names locals "$uid,*"] {
+	unset locals($key)
+    }
+    return [expr {($finito <= 0) ? "deny" : "accept"}]
+}
+
+proc ::Jabber::Subscribe::SendMsg {uid} {
+    global  wDlgs
+    variable locals   
+	
+    ::Jabber::NewMsg::Build $wDlgs(jsendmsg) -to $locals($uid,jid)
+}
+
+# Jabber::Subscribe::Doit --
+#
+#	Execute the subscription.
+
+proc ::Jabber::Subscribe::Doit {uid} {    
+    variable locals   
+    upvar ::Jabber::jstate jstate
+    
+    set jid $locals($uid,jid)
+    ::Jabber::Debug 2 "::Jabber::Subscribe::Doit jid=$jid, locals($uid,add)=$locals($uid,add), locals($uid,allow)=$locals($uid,allow)"
+    
+    # Accept (allow) or deny subscription.
+    if {$locals($uid,allow)} {
+	$jstate(jlib) send_presence -to $jid -type "subscribed"
+    } else {
+	$jstate(jlib) send_presence -to $jid -type "unsubscribed"
+    }
+	
+    # Add user to my roster. Send subscription request.	
+    if {$locals($uid,add)} {
+	set arglist {}
+	if {[string length $locals($uid,name)]} {
+	    lappend arglist -name $locals($uid,name)
+	}
+	if {($locals($uid,group) != "") && ($locals($uid,group) != "None")} {
+	    lappend arglist -groups [list $locals($uid,group)]
+	}
+	eval {$jstate(jlib) roster_set $jid ::Jabber::Subscribe::ResProc} \
+	  $arglist
+	$jstate(jlib) send_presence -to $jid -type "subscribe"
+    }
+    set locals($uid,finished) 1
+    destroy $locals($uid,wtop)
+}
+
+proc ::Jabber::Subscribe::Cancel {uid} {    
+    variable locals   
+    upvar ::Jabber::jstate jstate
+    
+    set jid $locals($uid,jid)
+
+    ::Jabber::Debug 2 "::Jabber::Subscribe::Cancel jid=$jid"
+    
+    # Deny presence to this user.
+    $jstate(jlib) send_presence -to $jid -type {unsubscribed}
+
+    set locals($uid,finished) 0
+    destroy $locals($uid,wtop)
+}
+
+# Jabber::Subscribe::ResProc --
+#
+#       This is our callback proc when setting the roster item from the
+#       subscription dialog. Catch any errors here.
+
+proc ::Jabber::Subscribe::ResProc {jlibName what} {
+    
+    upvar ::Jabber::jstate jstate
+    
+    ::Jabber::Debug 2 "::Jabber::Subscribe::ResProc: jlibName=$jlibName, what=$what"
+
+    if {[string equal $what "error"]} {
+	tk_messageBox -type ok -message "We got an error from the\
+	  Jabber::Subscribe::ResProc callback"
+    }
+    
+}
+
+#-------------------------------------------------------------------------------
