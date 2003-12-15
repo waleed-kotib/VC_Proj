681ba4c4f523bf0ef3416fe4147a0260ad58cadf
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2003-12-15 08:19:16 +0000

    * A wrapper for the label widget useful for theming.

---
+++ contrib/headlabel.tcl
@@ -0,0 +1,65 @@
+#  headlabel.tcl ---
+#  
+#      This file is just a wrapper for the label widget.
+#      Needed to make a separate widget since theming is easier.
+#      
+#  Copyright (c) 2003  Mats Bengtsson
+#  
+# $Id: headlabel.tcl,v 1.1 2003-12-15 08:19:16 matben Exp $
+#
+
+package provide headlabel 0.1
+
+namespace eval ::headlabel:: {
+
+    # The public interface.
+    namespace export headlabel
+    
+    option add *HeadLabel.Font          {Helvetica 16}
+    option add *HeadLabel.anchor        w
+    option add *HeadLabel.Background    #cecece
+    option add *HeadLabel.padX          10
+    option add *HeadLabel.padY          4
+                                        
+    variable widgetOptions
+    array set widgetOptions {
+	-anchor     {anchor          Anchor}
+	-font       {font            Font}
+	-background {background      Background}
+	-padx       {padX            Pad}
+	-pady       {padY            Pad}
+    }
+}
+
+# ::headlabel::headlabel --
+#
+#       Creates a headlabel widget
+#       
+# Arguments:
+#       w           widget path
+#       args        options for the label widget
+#       
+# Results:
+#       Widget path
+
+proc ::headlabel::headlabel {w args} {
+    variable widgetOptions
+    
+    frame $w -class HeadLabel
+    
+    # Replace label defaults with our own.
+    foreach name [array names widgetOptions] {
+	set optName [lindex $widgetOptions($name) 0]
+	set optClass [lindex $widgetOptions($name) 1]
+	set options($name) [option get $w $optName $optClass]
+    }
+    if {[llength $args] > 0}  {
+	array set options $args
+    }
+    
+    eval {label $w.l} [array get options]
+    pack $w.l -fill both -expand 1
+    return $w
+}
+
+#-------------------------------------------------------------------------------
