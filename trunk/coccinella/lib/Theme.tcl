2fd28baed64758e57a7767edcc95603560fd3aba
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2003-12-16 10:16:59 +0000

    * Collection of some procs to make theming easier.

---
+++ trunk/coccinella/lib/Theme.tcl
@@ -0,0 +1,73 @@
+# Theme.tcl --
+#
+#       Some utitilty procedures useful when theming widgets and UI.
+#       
+#  Copyright (c) 2003  Mats Bengtsson
+#  
+# $Id: Theme.tcl,v 1.1 2003-12-16 10:16:59 matben Exp $
+
+package provide Theme 1.0
+
+namespace eval ::Theme:: {
+
+}
+
+
+proc ::Theme::ReadPrefsFile { } {
+    global  this prefs
+    
+    if {[file exists $this(themePrefsPath)]} {
+	option readfile $this(themePrefsPath)
+    }
+    set prefs(themeName) [option get . themeName {}]   
+}
+
+proc ::Theme::SavePrefsFile { } {
+    global  this prefs
+    
+    # Work on a temporary file and switch later.
+    set tmpFile $this(themePrefsPath).tmp
+    if {[catch {open $tmpFile w} fid]} {
+	tk_messageBox -icon error -type ok -message \
+	  [FormatTextForMessageBox [::msgcat::mc messerrpreffile $tmpFile]]
+	return
+    }
+    
+    # Header information.
+    puts $fid "!\n!   User preferences for the theme name."
+    puts $fid "!   The data written at: [clock format [clock seconds]]\n!"
+    
+    puts $fid [format "%-24s\t%s" *themeName: $prefs(themeName)]
+    
+    close $fid
+    if {[catch {file rename -force $tmpFile $this(themePrefsPath)} msg]} {
+	tk_messageBox -type ok -message {Error renaming preferences file.}  \
+	  -icon error
+	return
+    }
+}
+
+proc ::Theme::PreLoadImages { } {
+    global  this
+    
+    foreach name [option get . ] {
+	::Theme::GetImage $name
+    }
+}
+
+proc ::Theme::GetImage {name} {
+    global  this
+	
+    if {[lsearch [image names] $name] == -1} {
+	foreach dir $this(imagePathList) {
+	    set f [file join $dir ${name}.gif]
+	    if {[file exists $f]} {
+		image create photo $name -file $f
+		break
+	    }
+	}
+    }
+    return $name
+}
+
+#-------------------------------------------------------------------------------
