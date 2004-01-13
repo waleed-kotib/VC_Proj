eb10a6dede1c29baa19e12c2328fde3dd0ccfe99
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2004-01-13 11:37:50 +0000

    * Rewritten to be multi instance. Removed tkwait which is a really bad thing.

--- trunk/coccinella/jabber/VCard.tcl
+++ trunk/coccinella/jabber/VCard.tcl
@@ -1,25 +1,23 @@
 #  VCard.tcl ---
 #  
-#      This file is part of the whiteboard application. 
+#      This file is part of The Coccinella application. 
 #      
 #  Copyright (c) 2001-2003  Mats Bengtsson
 #  
 #  See the README file for license, bugs etc.
 #  
-# $Id: VCard.tcl,v 1.11 2004-01-07 14:57:34 matben Exp $
+# $Id: VCard.tcl,v 1.12 2004-01-13 11:37:50 matben Exp $
 
 package provide VCard 1.0
 
 package require mactabnotebook
 
 namespace eval ::VCard::  {
-    
-    variable tmpPrefs
-    
-    # Variable to be used in tkwait.
-    variable finished
-    variable uid 1001
-    variable elem
+        
+    # Add all event hooks.
+    hooks::add closeWindowHook    ::VCard::CloseHook
+
+    variable uid 0
 }
 
 # VCard::Fetch --
@@ -28,44 +26,51 @@ namespace eval ::VCard::  {
 
 proc ::VCard::Fetch {type {jid {}}} {
     global  wDlgs
-
     variable uid
-    upvar ::Jabber::jstate jstate
 
     if {$type == "own"} {
-        set jid $jstate(mejid)
+        set jid [::Jabber::GetMyJid]
     }
-    set wtoplevel $wDlgs(jvcard)[incr uid]
-        
+    
+    # Keep a separate instance specific namespace for each VCard.
+    set nstoken [namespace current]::[incr uid]
+    namespace eval $nstoken {
+	variable elem
+	variable priv
+    }
+    upvar ${nstoken}::priv priv
+    
+    set priv(jid)  $jid
+    set priv(type) $type
+    set priv(w)    $wDlgs(jvcard)${uid}
+    
     # We should query the server for this and then fill in.
     ::Jabber::UI::SetStatusMessage [::msgcat::mc vcardget $jid]
-    $jstate(jlib) vcard_get $jid  \
-      [list ::VCard::FetchCallback $wtoplevel $type $jid]
+    ::Jabber::InvokeJlibCmd vcard_get $jid  \
+      [list [namespace current]::FetchCallback $nstoken]
 }
 
 # VCard::FetchCallback --
 #
 #       This is our callback from the 'vcard_get' procedure.
 
-proc ::VCard::FetchCallback {w type jid jlibName result theQuery} {
+proc ::VCard::FetchCallback {nstoken jlibName result theQuery} {
     
-    variable elem
-
     if {$result == "error"} {
         tk_messageBox -title [::msgcat::mc Error] -icon error -type ok \
           -message [FormatTextForMessageBox  \
           [::msgcat::mc vcarderrget $theQuery]]
         ::Jabber::UI::SetStatusMessage ""
+	::VCard::Free $nstoken
         return
     }
     ::Jabber::UI::SetStatusMessage [::msgcat::mc vcardrec]
     
     # The 'theQuery' now contains all the vCard data in a xml list.
-    catch {unset elem}
     if {[llength $theQuery]} {
-        ::VCard::ParseXmlList $theQuery [namespace current]::elem
+        ::VCard::ParseXmlList $theQuery ${nstoken}::elem
     }
-    Build $w $type $jid
+    Build $nstoken
 }
 
 # VCard::ParseXmlList --
@@ -135,37 +140,26 @@ proc ::VCard::ParseXmlList {subiq arrName} {
 #
 #   
 # Arguments:
-#       w      the toplevel window.
 #       type   "own" or "other"
 #       
 # Results:
 #       shows dialog.
 
-proc ::VCard::Build {w type jid} {
-    global  this prefs
+proc ::VCard::Build {nstoken} {
+    global  this prefs wDlgs
     
-    variable finished
-    variable tmpPrefs
-    variable elem
-    variable wemails
-    variable vcardjid
-    variable wdesctxt
-    upvar ::Jabber::jstate jstate
-    
-    set finished 0
+    upvar ${nstoken}::elem elem
+    upvar ${nstoken}::priv priv
+
     set anyChange 0
+    set w    $priv(w)
+    set jid  $priv(jid)
+    set type $priv(type)
     
-    if {[winfo exists $w]} {
-        return
-    }
     ::UI::Toplevel $w -macstyle documentProc -usemacmainmenu 1
     wm title $w [::msgcat::mc {vCard Info}]
-    
-    # Toplevel menu for mac only.
-    if {[string match "mac*" $this(platform)]} {
-	$w configure -menu [::Jabber::UI::GetRosterWmenu]
-    }
-    set vcardjid $jid
+
+    set priv(vcardjid) $jid
     
     # Global frame.
     pack [frame $w.frall -borderwidth 0 -relief raised] -fill both -expand 1
@@ -192,30 +186,25 @@ proc ::VCard::Build {w type jid} {
     label $pbi.first -text [::msgcat::mc {First name}]
     label $pbi.middle -text [::msgcat::mc Middle]
     label $pbi.fam -text [::msgcat::mc {Last name}]
-    entry $pbi.efirst -width 16  \
-      -textvariable [namespace current]::elem(n_given)
-    entry $pbi.emiddle -width 4  \
-      -textvariable [namespace current]::elem(n_middle)
-    entry $pbi.efam -width 18  \
-      -textvariable [namespace current]::elem(n_family)
+    entry $pbi.efirst -width 16 -textvariable ${nstoken}::elem(n_given)
+    entry $pbi.emiddle -width 4 -textvariable ${nstoken}::elem(n_middle)
+    entry $pbi.efam -width 18   -textvariable ${nstoken}::elem(n_family)
     grid $pbi.first $pbi.middle $pbi.fam -sticky w
     grid $pbi.efirst $pbi.emiddle $pbi.efam -sticky ew
     
     # Other part.
-    label $pbi.nick -text "[::msgcat::mc {Nick name}]:"
-    label $pbi.email -text "[::msgcat::mc {Email address}]:"
-    label $pbi.jid -text "[::msgcat::mc {Jabber address}]:"
-    entry $pbi.enick -textvariable [namespace current]::elem(nickname)
-    entry $pbi.eemail  \
-      -textvariable [namespace current]::elem(email_internet_pref)
-    entry $pbi.ejid -state disabled \
-      -textvariable [namespace current]::vcardjid
-    grid $pbi.nick -column 0 -row 2 -sticky e
-    grid $pbi.enick -column 1 -row 2 -sticky news -columnspan 2
-    grid $pbi.email -column 0 -row 3 -sticky e
+    label $pbi.nick   -text "[::msgcat::mc {Nick name}]:"
+    label $pbi.email  -text "[::msgcat::mc {Email address}]:"
+    label $pbi.jid    -text "[::msgcat::mc {Jabber address}]:"
+    entry $pbi.enick  -textvariable ${nstoken}::elem(nickname)
+    entry $pbi.eemail -textvariable ${nstoken}::elem(email_internet_pref)
+    entry $pbi.ejid   -textvariable ${nstoken}::priv(vcardjid) -state disabled
+    grid $pbi.nick   -column 0 -row 2 -sticky e
+    grid $pbi.enick  -column 1 -row 2 -sticky news -columnspan 2
+    grid $pbi.email  -column 0 -row 3 -sticky e
     grid $pbi.eemail -column 1 -row 3 -sticky news -columnspan 2
-    grid $pbi.jid -column 0 -row 4 -sticky e
-    grid $pbi.ejid -column 1 -row 4 -sticky news -columnspan 2
+    grid $pbi.jid    -column 0 -row 4 -sticky e
+    grid $pbi.ejid   -column 1 -row 4 -sticky news -columnspan 2
     
     # Description part.
     label $pbi.ldes -text "[::msgcat::mc Description]:"    
@@ -248,7 +237,7 @@ proc ::VCard::Build {w type jid} {
     } {
         label $pbp.l$tag -text "[::msgcat::mc $name]:"
         entry $pbp.e$tag -width 28  \
-          -textvariable [namespace current]::elem($tag)
+          -textvariable ${nstoken}::elem($tag)
         grid $pbp.l$tag $pbp.e$tag -sticky e
     }
     label $pbp.frmt -text {Format mm/dd/yyyy}
@@ -283,7 +272,7 @@ proc ::VCard::Build {w type jid} {
     } {
         label $pbh.l$tag -text "[::msgcat::mc $name]:"
         entry $pbh.e$tag -width 28  \
-          -textvariable [namespace current]::elem($tag)
+          -textvariable ${nstoken}::elem($tag)
         grid $pbh.l$tag $pbh.e$tag -sticky e
     }
     
@@ -306,8 +295,7 @@ proc ::VCard::Build {w type jid} {
         {Tel (fax)}       tel_fax_work
     } {
         label $pbw.l$tag -text "[::msgcat::mc $name]:"
-        entry $pbw.e$tag -width 28  \
-          -textvariable [namespace current]::elem($tag)
+        entry $pbw.e$tag -width 28 -textvariable ${nstoken}::elem($tag)
         grid $pbw.l$tag $pbw.e$tag -sticky e
     }
 
@@ -320,7 +308,7 @@ proc ::VCard::Build {w type jid} {
                 }
             }
         }
-        $wemails configure -state disabled
+        $wemails  configure -state disabled
         $wdesctxt configure -state disabled
     }
         
@@ -328,38 +316,36 @@ proc ::VCard::Build {w type jid} {
     frame $w.frbot -borderwidth 0
     if {$type == "own"} {
         pack [button $w.btsave -text [::msgcat::mc Save] -width 8  \
-          -default active -command [namespace current]::PushBtSetVCard] \
+          -default active -command [list [namespace current]::SetVCard $nstoken]] \
           -in $w.frbot -side right -padx 5 -pady 5
         pack [button $w.btcancel -text [::msgcat::mc Cancel] -width 8   \
-          -command "set [namespace current]::finished 0"]  \
+          -command [list [namespace current]::Close $nstoken]]  \
           -in $w.frbot -side right -padx 5 -pady 5
     } else {
         pack [button $w.btcancel -text [::msgcat::mc Close] -width 8 \
-          -command "set [namespace current]::finished 0"]  \
+          -command [list [namespace current]::Close $nstoken]]  \
           -in $w.frbot -side right -padx 5 -pady 5
     }
     pack $w.frbot -side top -fill both -expand 1 -in $frall -padx 8 -pady 6
 
+    set priv(wemails)  $wemails
+    set priv(wdesctxt) $wdesctxt
+    
+    set nwin [llength [::UI::GetPrefixedToplevels $wDlgs(jvcard)]]
+    if {$nwin == 1} {
+	::UI::SetWindowGeometry $w $wDlgs(jvcard)
+    }
     wm resizable $w 0 0
     focus $w
-    catch {grab $w}
-    #bind $w <Return> "$w.btsave invoke"
-    tkwait variable [namespace current]::finished
-    
-    # Clean up.
-    catch {grab release $w}
-    destroy $w
-
 }
 
-proc ::VCard::PushBtSetVCard { }  {
+proc ::VCard::SetVCard {nstoken}  {
 
-    variable finished
-    variable elem
-    variable wemails
-    variable wdesctxt
-    upvar ::Jabber::jstate jstate
+    upvar ${nstoken}::elem elem
     
+    set wemails  $priv(wemails)
+    set wdesctxt $priv(wdesctxt)
+
     if {($elem(n_given) != "") && ($elem(n_family) != "")} {
         set elem(fn) "$elem(n_given) $elem(n_family)"
     }
@@ -370,13 +356,45 @@ proc ::VCard::PushBtSetVCard { }  {
     
     # Collect all non empty entries, and send a vCard set.
     set argList {}
-    foreach key [array names elem] {
-	if {$elem($key) != ""} {
-	    lappend argList -$key $elem($key)
+    foreach {key value} [array get elem] {
+	if {[string length $value]} {
+	    lappend argList -$key $value
 	}
     }
-    eval {$jstate(jlib) vcard_set ::VCard::SetVCardCallback} $argList
-    set finished 1
+    eval {::Jabber::InvokeJlibCmd vcard_set ::VCard::SetVCardCallback} $argList
+    ::VCard::Close $nstoken
+}
+
+proc ::VCard::CloseHook {wclose} {
+    global  wDlgs
+    
+    if {[string match $wDlgs(jvcard)* $wclose]} {
+	set nstoken [::VCard::GetNSTokenFrom w $wclose]
+	if {$nstoken != ""} {
+	    ::VCard::Close $nstoken
+	}
+    }   
+}
+
+proc ::VCard::GetNSTokenFrom {key pattern} {
+    
+    foreach ns [namespace children [namespace current]] {
+	set val [set ${ns}::priv($key)]
+	if {[string match $pattern $val]} {
+	    return $ns
+	}
+    }
+    return ""
+}
+
+proc ::VCard::Close {nstoken} {
+    global  wDlgs
+    
+    upvar ${nstoken}::priv priv
+    
+    ::UI::SaveWinGeom $wDlgs(jvcard) $priv(w)
+    destroy $priv(w)
+    ::VCard::Free $nstoken
 }
 
 # VCard::SetVCardCallback --
@@ -391,6 +409,11 @@ proc ::VCard::SetVCardCallback {jlibName type theQuery} {
 	  "Failed setting the vCard. The result was: $theQuery"]	  
 	return
     }
+}
+
+proc ::VCard::Free {nstoken} {
+    
+    namespace delete $nstoken
 }
 
 #-------------------------------------------------------------------------------
