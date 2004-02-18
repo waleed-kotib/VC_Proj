13ef97e955cb180d47b550a6e962f945d77ef259
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2004-02-18 08:56:22 +0000

    * Just more code.

--- trunk/coccinella/contrib/svg2can.tcl
+++ trunk/coccinella/contrib/svg2can.tcl
@@ -4,7 +4,7 @@
 #      
 #  Copyright (c) 2004  Mats Bengtsson
 #
-# $Id: svg2can.tcl,v 1.1 2004-02-17 07:43:32 matben Exp $
+# $Id: svg2can.tcl,v 1.2 2004-02-18 08:56:22 matben Exp $
 # 
 # ########################### USAGE ############################################
 #
@@ -28,7 +28,7 @@
 # 
 #       xmllist = {tag attrlist isempty cdata {child1 child2 ...}}
 
-# We need URN encoding for the file path in images. From my whiteboard code.
+# We need URN decoding for the file path in images. From my whiteboard code.
 
 package require uriencode
 
@@ -39,26 +39,62 @@ namespace eval svg2can {
 }
 
 
-proc svg2can::parseelement {xmllist} {
 
+proc svg2can::parsesvgdocument {xmllist args} {
+        
+    set ans {}
+    foreach c [getchildren $xmllist] {
+	lappend ans [parseelement $c]
+    }
+    return $ans
+}
+
+proc svg2can::parseelement {xmllist args} {
+
+    set cmd {}
     set tag [gettag $xmllist]
     
     switch -- $tag {
-	line - circle - rect - polyline - polygon {
-	    set cmd [parse${tag} $xmllist]
+	circle - image - line - polyline - polygon - rect {
+	    lappend cmd [parse${tag} $xmllist]
+	}
+	g {
+	    foreach c [getchildren $xmllist] {
+		set ctag [gettag $xmllist]
+		lappend cmd [parse${ctag} $xmllist]
+	    }	    
 	}
     }
     return $cmd
 }
 
+# svg2can::parseline, parsecircle, parseellipse, parserect, parsepolyline,
+#   parsepolygon, parseimage --
+# 
+#       Makes the necessary canvas commands needed to reproduce the
+#       svg element.
+#       
+# Arguments:
+#       xmllist
+#       
+# Results:
+#       canvas create command without the widgetPath.
+
 proc svg2can::parseline {xmllist} {
     
-    set opts {...}
+    set opts {}
     set coords {0 0 0 0}
+    set presentationAttr {}
     
     foreach {key value} [getattr $xmllist] {
 	
 	switch -- $key {
+	    id {
+		lappend opts -tags $value
+	    }
+	    style {
+		set opts [StyleToOpts line [StyleAttrToList $value]]
+	    }
 	    x1 {
 		lset coords 0 $value
 	    }
@@ -71,23 +107,20 @@ proc svg2can::parseline {xmllist} {
 	    y2 {
 		lset coords 3 $value
 	    }
-	    id {
-		lappend opts -tags $value
-	    }
-	    style {
-		set opts [style2opts $value]
-	    }
 	    default {
-		# empty
+		lappend presentationAttr $key $value
 	    }
 	}
     }
+    set opts [MergePresentationAttr line $opts $presentationAttr]    
     return [concat create line $coords $opts]
 }
 
 proc svg2can::parsecircle {xmllist} {
     
-    set opts {...}
+    # SVG and canvas have different defaults.
+    set opts {-outline "" -fill black}
+    set presentationAttr {}
     set cx 0
     set cy 0
     set r 0
@@ -102,21 +135,24 @@ proc svg2can::parsecircle {xmllist} {
 		lappend opts -tags $value
 	    }
 	    style {
-		set opts [style2opts $value]
+		set opts [StyleToOpts circle [StyleAttrToList $value]]
 	    }
 	    default {
-		# empty
+		lappend presentationAttr $key $value
 	    }
 	}
     }
     set coords [list [expr $cx - $r] [expr $cy - $r] \
       [expr $cx + $r] [expr $cy + $r]]
+    set opts [MergePresentationAttr circle $opts $presentationAttr]
     return [concat create oval $coords $opts]
 }
 
 proc svg2can::parseellipse {xmllist} {
     
-    set opts {...}
+    # SVG and canvas have different defaults.
+    set opts {-outline "" -fill black}
+    set presentationAttr {}
     set cx 0
     set cy 0
     set rx 0
@@ -132,40 +168,43 @@ proc svg2can::parseellipse {xmllist} {
 		lappend opts -tags $value
 	    }
 	    style {
-		set opts [style2opts $value]
+		set opts [StyleToOpts ellipse [StyleAttrToList $value]]
 	    }
 	    default {
-		# empty
+		lappend presentationAttr $key $value
 	    }
 	}
     }
     set coords [list [expr $cx - $rx] [expr $cy - $ry] \
       [expr $cx + $rx] [expr $cy + $ry]]
+    set opts [MergePresentationAttr ellipse $opts $presentationAttr]
     return [concat create oval $coords $opts]
 }
 
 proc svg2can::parserect {xmllist} {
     
-    set opts {...}
+    # SVG and canvas have different defaults.
+    set opts {-outline "" -fill black}
     set coords {0 0 0 0}
+    set presentationAttr {}
     
     foreach {key value} [getattr $xmllist] {
 	
 	switch -- $key {
-	    x - y - width - height {
-		set $key $value
+	    id {
+		lappend opts -tags $value
 	    }
 	    rx - ry {
 		# unsupported :-(
 	    }
-	    id {
-		lappend opts -tags $value
-	    }
 	    style {
-		set opts [style2opts $value]
+		set opts [StyleToOpts rect [StyleAttrToList $value]]
+	    }
+	    x - y - width - height {
+		set $key $value
 	    }
 	    default {
-		# empty
+		lappend presentationAttr $key $value
 	    }
 	}
     }
@@ -179,30 +218,107 @@ proc svg2can::parserect {xmllist} {
 	lset coords 2 [expr [lindex $coords 0] + $width]
     }
     if {[info exists height]} {
-	lset coords 2 [expr [lindex $coords 1] + $height]
+	lset coords 3 [expr [lindex $coords 1] + $height]
     }
+    set opts [MergePresentationAttr rect $opts $presentationAttr]
     return [concat create rectangle $coords $opts]
 }
 
 proc svg2can::parsepolyline {xmllist} {
     
-    
+    set coords {}
+    set opts {}
+    set presentationAttr {}
+
+    foreach {key value} [getattr $xmllist] {
+	
+	switch -- $key {
+	    points {
+		set coords [PointsToList $value]
+	    }
+	    id {
+		lappend opts -tags $value
+	    }
+	    style {
+		set opts [StyleToOpts polyline [StyleAttrToList $value]]
+	    }
+	    default {
+		lappend presentationAttr $key $value
+	    }
+	}
+    }
+    set opts [MergePresentationAttr polyline $opts $presentationAttr]
+    return [concat create line $coords $opts]
 }
 
 proc svg2can::parsepolygon {xmllist} {
     
+    set coords {}
+    set opts {}
+    set presentationAttr {}
+
+    foreach {key value} [getattr $xmllist] {
+	
+	switch -- $key {
+	    id {
+		lappend opts -tags $value
+	    }
+	    points {
+		set coords [PointsToList $value]
+	    }
+	    style {
+		set opts [StyleToOpts polygon [StyleAttrToList $value]]
+	    }
+	    default {
+		lappend presentationAttr $key $value
+	    }
+	}
+    }
+    set opts [MergePresentationAttr polygon $opts $presentationAttr]
+    return [concat create polygon $coords $opts]
+}
+
+proc svg2can::parseimage {xmllist} {
     
+    set x 0
+    set y 0    
+    set presentationAttr {}
+
+    foreach {key value} [getattr $xmllist] {
+	
+	switch -- $key {
+	    id {
+		lappend opts -tags $value
+	    }
+	    style {
+		set opts [StyleToOpts polygon [StyleAttrToList $value]]
+	    }
+	    x - y {
+		set $key $value
+	    }
+	    xlink:href {
+		set uri $value
+	    }
+	    default {
+		lappend presentationAttr $key $value
+	    }
+	}
+    }
+    set opts [MergePresentationAttr polygon $opts $presentationAttr]
+    lappend opts -image \[image create photo -file $uri]
+    return [concat create image $x $y $opts]
+}
 }
 
 proc svg2can::parseColor {color} {
     
-    if {[regexp {rgb\(([0-9]{1,3})%, +([0-9]{1,3})%, +([0-9]{1,3})%\)}  \
+    if {[regexp {rgb\(([0-9]{1,3})%, *([0-9]{1,3})%, *([0-9]{1,3})%\)}  \
       $color match r g b]} {
 	set col #
 	foreach c [list $r $g $b] {
-	    append col [format %2x [expr round(2.55 * $c)]]
+	    append col [format %02x [expr round(2.55 * $c)]]
 	}
-    } elseif {[regexp {rgb\(([0-9]{1,3}), +([0-9]{1,3}), +([0-9]{1,3})\)}  \
+    } elseif {[regexp {rgb\(([0-9]{1,3}), *([0-9]{1,3}), *([0-9]{1,3})\)}  \
       $color match r g b]} {
 	set col #
 	foreach c [list $r $g $b] {
@@ -214,13 +330,25 @@ proc svg2can::parseColor {color} {
     return $col
 }
 
-proc svg2can::style2opts {style} {
+# svg2can::StyleToOpts --
+# 
+#       Takes the style attribute as a list and parses it into
+#       resonable canvas drawing options.
+#       
+# Arguments:
+#       type        svg item type
+#       styleList
+#       
+# Results:
+#       list of canvas options
+
+proc svg2can::StyleToOpts {type styleList} {
     
     set opts {}
     set font {Helvetica 12 normal}
     set haveFont 0
     
-    foreach {key value} [split $style ":;"] {
+    foreach {key value} $styleList {
 	
 	switch -- $key {
 	    font-family {
@@ -244,7 +372,14 @@ proc svg2can::style2opts {style} {
 		set haveFont 1
 	    }
 	    stroke {
-		lappend opts -outline/-fill [parseColor $value]
+		switch -- $type {
+		    rect - circle - ellipse {
+			lappend opts -outline [parseColor $value]
+		    }
+		    default {
+			lappend opts -fill [parseColor $value]
+		    }
+		}
 	    }
 	    stroke-dasharray {
 		set dash [split $value ,]
@@ -252,8 +387,21 @@ proc svg2can::style2opts {style} {
 		    set dash [concat $dash $dash]
 		}
 	    }
+	    stroke-linecap {	
+		# canvas: butt (D), projecting , round 
+		# svg:    butt (D), square, round
+		if {[string equal $value "square"]} {
+		    lappend opts -capstyle "projecting"
+		}
+		if {![string equal $value "butt"]} {
+		    lappend opts -capstyle $value
+		}
+	    }
 	    stroke-linejoin {
 		lappend opts -joinstyle $value
+	    }
+	    stroke-miterlimit {
+		
 	    }
 	    stroke-opacity {
 		if {[expr {$value == 0}]} {
@@ -277,7 +425,31 @@ proc svg2can::style2opts {style} {
     return $opts
 }
 
-proc ::svg2can::MapNoneToEmpty {val} {
+# svg2can::MergePresentationAttr --
+# 
+#       Let the style attribute override the presentaion attributes.
+
+proc svg2can::MergePresentationAttr {type opts presentationAttr} {
+    
+    if {[llength $presentationAttr]} {
+	array set optsArr [array get [StyleToOpts $type $presentationAttr]]
+	array set optsArr [array get $opts]
+	set opts [array get optsArr]
+    }
+    return $opts
+}
+
+proc svg2can::StyleAttrToList {style} {
+    
+    return [split [string trim [string map {" " ""} $style] \;] :\;]
+}
+
+proc svg2can::PointsToList {points} {
+    
+    return [string map {, " "} $points]
+}
+
+proc svg2can::MapNoneToEmpty {val} {
 
     if {[string equal $val "none"]} {
 	return ""
@@ -285,6 +457,10 @@ proc ::svg2can::MapNoneToEmpty {val} {
 	return $val
     }
 }
+
+# svg2can::gettag, getattr, getcdata, getchildren --
+# 
+#       Accesor functions to the specific things in a xmllist.
 
 proc svg2can::gettag {xmllist} { 
     return [lindex $xmllist 0]
