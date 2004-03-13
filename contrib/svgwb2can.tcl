d7c7f72212ca2340445fc0b14ea3cd71ffee96d3
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2004-03-13 15:18:32 +0000

    * Prototypes for SVG inspired new protocol.

---
+++ contrib/svgwb2can.tcl
@@ -0,0 +1,16 @@
+#  svgwb2can.tcl ---
+#  
+#      This file provides translation from canvas commands to ...
+#      
+#  Copyright (c) 2004  Mats Bengtsson
+#
+# $Id: svgwb2can.tcl,v 1.1 2004-03-13 15:18:32 matben Exp $
+
+package require svg2can
+
+package provide svgwb2can 1.0
+
+namespace eval svgwb2can {
+
+
+}
