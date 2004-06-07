7be466413d280c6c29c2b60a07fcb31e8b7a3281
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2004-06-07 06:38:26 +0000

    * Moved a few files.

--- plugins/cmpntIndex.tcl
+++ plugins/cmpntIndex.tcl
@@ -1,4 +1,13 @@
 # See contrib/component.tcl for explanations.
 #
 
-component::attempt Speech [file join $dir Speech.tcl] ::Speech::Load    
+component::attempt Speech     [file join $dir Speech.tcl]     ::Speech::Load    
+component::attempt ImageMagic [file join $dir ImageMagic.tcl] ::ImageMagic::Init
+
+# Problem to determine if app hidden or not!
+if {[string equal $::tcl_platform(platform) "unix"] && [string equal [tk windowingsystem] "aqua"]} {
+    #component::register CarbonNotification [file join $dir CarbonNotification.tcl] ::CarbonNotification::Init
+}
+
+# This is just an example plugin. Uncomment to test.
+#component::attempt ComponentExample [file join $dir ComponentExample.tcl] ::ComponentExample::Init
