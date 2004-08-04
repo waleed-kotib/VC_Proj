3c191ea75512d6c72e449a001b259bc5ea8183f0
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2004-08-04 13:18:45 +0000

    * Updated to 3.1a3?  * Fixed problem with infinite loop in -mccommand.

--- bin/macosx/PowerMacintosh/QuickTimeTcl3.1/pkgIndex.tcl
+++ bin/macosx/PowerMacintosh/QuickTimeTcl3.1/pkgIndex.tcl
@@ -1,1 +1,2 @@
-package ifneeded QuickTimeTcl 3.1 [list load [file join $dir QuickTimeTcl3.1.dylib]]
+package ifneeded QuickTimeTcl 3.1 [list load [file join $dir QuickTimeTcl3.1.dylib]; \
+  source [file join $dir movie.tcl]]
