51a8432da8d20edbda9e6e308219143c433ce8a4
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2004-10-25 07:20:34 +0000

    * Updated libsnack.so to 2.2.8  * Modified pkgIndex to check if sound port is busy before loading snack.

--- bin/unix/Linux/i686/snack2.2/pkgIndex.tcl
+++ bin/unix/Linux/i686/snack2.2/pkgIndex.tcl
@@ -1,8 +1,12 @@
 #
 # Immediate loading of Snack because of item types etc.
 #
-# http://www.wjduquette.com/tcl/namespaces.html
-#
+# Jason Tang: http://mini.net/tcl/2647
+#  libsnack blocks if sound device is in use; so detect by trying to open /dev/dsp 
+# Mats Bengtsson: don't know how general this fix is.
 
-package ifneeded snack 2.2 "[list load [file join $dir libsnack.so]];[list source [file join $dir snack.tcl]]"
-
+if {![catch {open /dev/dsp "WRONLY NONBLOCK"} f]} {
+    close $f
+    package ifneeded snack 2.2 "[list load [file join $dir libsnack.so]]; \
+      [list source [file join $dir snack.tcl]]"
+}
