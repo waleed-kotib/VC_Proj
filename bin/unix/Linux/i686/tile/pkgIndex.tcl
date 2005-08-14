65750d6407b5dda8aa0b81e8a0ce745060b40753
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2005-08-14 07:27:15 +0000

    added

---
+++ bin/unix/Linux/i686/tile/pkgIndex.tcl
@@ -0,0 +1,7 @@
+if {[catch {package require Tcl 8.4}]} return
+
+# Do not get staticalliy linked tile from here.
+if {[lsearch -exact [info loaded] {{} tile}] >= 0} return
+if {[package vcompare [info patchlevel] 8.4.6] < 0} return
+
+package ifneeded tile 0.6.3  [list load [file join $dir libtile0.6.so] tile]
