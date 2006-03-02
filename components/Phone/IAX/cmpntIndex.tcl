261045f13f1d32a26c988842e8a764cb9306e718
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2006-03-02 07:19:35 +0000

    * Commented out all cmpntIndex entries for Phone stuff.

--- components/Phone/IAX/cmpntIndex.tcl
+++ components/Phone/IAX/cmpntIndex.tcl
@@ -1,5 +1,5 @@
 # See contrib/component.tcl for explanations.
 #
 
-component::attempt IAX  [file join $dir Iax.tcl] ::Iax::Init
-component::attempt JingleIAX  [file join $dir JingleIAX.tcl] ::JingleIAX::Init
+#component::attempt IAX  [file join $dir Iax.tcl] ::Iax::Init
+#component::attempt JingleIAX  [file join $dir JingleIAX.tcl] ::JingleIAX::Init
