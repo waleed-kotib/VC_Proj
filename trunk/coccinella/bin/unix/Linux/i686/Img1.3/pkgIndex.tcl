d6409be7e033fc9ee19631f68fca7ef5bb8a27dc
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2006-05-16 06:33:26 +0000

    * Upgraded to latest Img which should solve crash problems.

--- trunk/coccinella/bin/unix/Linux/i686/Img1.3/pkgIndex.tcl
+++ trunk/coccinella/bin/unix/Linux/i686/Img1.3/pkgIndex.tcl
@@ -1,62 +1,15 @@
-# Tcl package index file - handcrafted
+package ifneeded zlibtcl 1.2.3  [list load [file join $dir libzlibtcl1.2.3.so]]
+package ifneeded pngtcl 1.2.6  [list load [file join $dir libpngtcl1.2.6.so]]
+package ifneeded tifftcl 3.6.1  [list load [file join $dir libtifftcl3.6.1.so]]
+package ifneeded jpegtcl 1.0  [list load [file join $dir libjpegtcl1.0.so]]
+# -*- tcl -*- Tcl package index file
+# --- --- --- Handcrafted, final generation by configure.
 #
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded zlibtcl 1.0 [list load [file join $dir libzlibtcl1.0.so]]
-
-# distinguish static and dyn variants, later.
-if {0} {
-package ifneeded zlibtcl 1.0 [string map [list @dir@ $dir] \
-"if {[catch {load [file join @dir@ libzlibtcl1.0.so]}]} {
-    load [file join @dir@ libzlibtcl1.0.so]
-}"]
-}
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-#package ifneeded pngtcl 1.0 [list load [file join $dir libpngtcl1.0.so]]
-
-# distinguish static and dyn variants, later.
-if {0} {
-package ifneeded pngtcl 1.0 [string map [list @dir@ $dir] \
-"if {[catch {load [file join @dir@ libpngtcl1.0.so]}]} {
-    load [file join @dir@ libpngtcl1.0.so]
-}"]
-}
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded tifftcl 1.0 [list load [file join $dir libtifftcl1.0.so]]
-
-# distinguish static and dyn variants, later.
-if {0} {
-package ifneeded tifftcl 1.0 [string map [list @dir@ $dir] \
-"if {[catch {load [file join @dir@ libtifftcl1.0.so]}]} {
-    load [file join @dir@ libtifftcl1.0.so]
-}"]
-}
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded jpegtcl 1.0 [list load [file join $dir libjpegtcl1.0.so]]
-
-# distinguish static and dyn variants, later.
-if {0} {
-package ifneeded jpegtcl 1.0 [string map [list @dir@ $dir] \
-"if {[catch {load [file join @dir@ libjpegtcl1.0.so]}]} {
-    load [file join @dir@ libjpegtcl1.0.so]
-}"]
-}
-# -*- tcl -*- Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
+# $Id: pkgIndex.tcl,v 1.5 2006-05-16 06:33:26 matben Exp $
 
 package ifneeded img::base 1.3 [list load [file join $dir libtkimg1.3.so]]
 
-package ifneeded Img   1.3 {
+package ifneeded Img 1.3 {
     # Compatibility hack. When asking for the old name of the package
     # then load all format handlers and base libraries provided by tkImg.
     # Actually we ask only for the format handlers, the required base
@@ -87,83 +40,19 @@ package ifneeded Img   1.3 {
 
     package provide Img 1.3
 }
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::bmp" 1.3 [list load [file join $dir libtkimgbmp1.3.so]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::gif" 1.3 [list load [file join $dir libtkimggif1.3.so]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::ico" 1.3 [list load [file join $dir libtkimgico1.3.so]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::jpeg" 1.3 [list load [file join $dir libtkimgjpeg1.3.so]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::pcx" 1.3 [list load [file join $dir libtkimgpcx1.3.so]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::pixmap" 1.3 [list load [file join $dir libtkimgpixmap1.3.so]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::png" 1.3 [list load [file join $dir libtkimgpng1.3.so]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::ppm" 1.3 [list load [file join $dir libtkimgppm1.3.so]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::ps" 1.3 [list load [file join $dir libtkimgps1.3.so]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::sgi" 1.3 [list load [file join $dir libtkimgsgi1.3.so]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::sun" 1.3 [list load [file join $dir libtkimgsun1.3.so]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::tga" 1.3 [list load [file join $dir libtkimgtga1.3.so]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::tiff" 1.3 [list load [file join $dir libtkimgtiff1.3.so]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::window" 1.3 [list load [file join $dir libtkimgwindow1.3.so]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::xbm" 1.3 [list load [file join $dir libtkimgxbm1.3.so]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.4 2006-05-08 07:46:18 matben Exp $
-
-package ifneeded "img::xpm" 1.3 [list load [file join $dir libtkimgxpm1.3.so]]
+package ifneeded img::bmp 1.3  [list load [file join $dir libtkimgbmp1.3.so]]
+package ifneeded img::gif 1.3  [list load [file join $dir libtkimggif1.3.so]]
+package ifneeded img::ico 1.3  [list load [file join $dir libtkimgico1.3.so]]
+package ifneeded img::jpeg 1.3  [list load [file join $dir libtkimgjpeg1.3.so]]
+package ifneeded img::pcx 1.3  [list load [file join $dir libtkimgpcx1.3.so]]
+package ifneeded img::pixmap 1.3  [list load [file join $dir libtkimgpixmap1.3.so]]
+package ifneeded img::png 1.3  [list load [file join $dir libtkimgpng1.3.so]]
+package ifneeded img::ppm 1.3  [list load [file join $dir libtkimgppm1.3.so]]
+package ifneeded img::ps 1.3  [list load [file join $dir libtkimgps1.3.so]]
+package ifneeded img::sgi 1.3  [list load [file join $dir libtkimgsgi1.3.so]]
+package ifneeded img::sun 1.3  [list load [file join $dir libtkimgsun1.3.so]]
+package ifneeded img::tga 1.3  [list load [file join $dir libtkimgtga1.3.so]]
+package ifneeded img::tiff 1.3  [list load [file join $dir libtkimgtiff1.3.so]]
+package ifneeded img::window 1.3  [list load [file join $dir libtkimgwindow1.3.so]]
+package ifneeded img::xbm 1.3  [list load [file join $dir libtkimgxbm1.3.so]]
+package ifneeded img::xpm 1.3  [list load [file join $dir libtkimgxpm1.3.so]]
