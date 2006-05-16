eb54797ad03dc708329831720e55b837ef0e05fb
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2006-05-16 06:39:52 +0000

    * Updated to latest Img to avoid crashes.

--- trunk/coccinella/bin/windows/intel/img1.3/pkgIndex.tcl
+++ trunk/coccinella/bin/windows/intel/img1.3/pkgIndex.tcl
@@ -1,62 +1,15 @@
-# Tcl package index file - handcrafted
+package ifneeded zlibtcl 1.2.3  [list load [file join $dir zlibtcl123.dll]]
+package ifneeded pngtcl 1.2.6  [list load [file join $dir pngtcl126.dll]]
+package ifneeded tifftcl 3.6.1  [list load [file join $dir tifftcl361.dll]]
+package ifneeded jpegtcl 1.0  [list load [file join $dir jpegtcl10.dll]]
+# -*- tcl -*- Tcl package index file
+# --- --- --- Handcrafted, final generation by configure.
 #
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded zlibtcl 1.0 [list load [file join $dir zlibtcl10.dll]]
-
-# distinguish static and dyn variants, later.
-if {0} {
-package ifneeded zlibtcl 1.0 [string map [list @dir@ $dir] \
-"if {[catch {load [file join @dir@ zlibtcl10.dll]}]} {
-    load [file join @dir@ zlibtcl10.dll]
-}"]
-}
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded pngtcl 1.0 [list load [file join $dir pngtcl10.dll]]
-
-# distinguish static and dyn variants, later.
-if {0} {
-package ifneeded pngtcl 1.0 [string map [list @dir@ $dir] \
-"if {[catch {load [file join @dir@ pngtcl10.dll]}]} {
-    load [file join @dir@ pngtcl10.dll]
-}"]
-}
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded tifftcl 1.0 [list load [file join $dir tifftcl10.dll]]
-
-# distinguish static and dyn variants, later.
-if {0} {
-package ifneeded tifftcl 1.0 [string map [list @dir@ $dir] \
-"if {[catch {load [file join @dir@ tifftcl10.dll]}]} {
-    load [file join @dir@ tifftcl10.dll]
-}"]
-}
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded jpegtcl 1.0 [list load [file join $dir jpegtcl10.dll]]
-
-# distinguish static and dyn variants, later.
-if {0} {
-package ifneeded jpegtcl 1.0 [string map [list @dir@ $dir] \
-"if {[catch {load [file join @dir@ jpegtcl10.dll]}]} {
-    load [file join @dir@ jpegtcl10.dll]
-}"]
-}
-# -*- tcl -*- Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
+# $Id: pkgIndex.tcl,v 1.4 2006-05-16 06:39:52 matben Exp $
 
 package ifneeded img::base 1.3 [list load [file join $dir tkimg13.dll]]
 
-package ifneeded Img   1.3 {
+package ifneeded Img 1.3 {
     # Compatibility hack. When asking for the old name of the package
     # then load all format handlers and base libraries provided by tkImg.
     # Actually we ask only for the format handlers, the required base
@@ -87,83 +40,19 @@ package ifneeded Img   1.3 {
 
     package provide Img 1.3
 }
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::bmp" 1.3 [list load [file join $dir tkimgbmp13.dll]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::gif" 1.3 [list load [file join $dir tkimggif13.dll]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::ico" 1.3 [list load [file join $dir tkimgico13.dll]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::jpeg" 1.3 [list load [file join $dir tkimgjpeg13.dll]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::pcx" 1.3 [list load [file join $dir tkimgpcx13.dll]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::pixmap" 1.3 [list load [file join $dir tkimgpixmap13.dll]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::png" 1.3 [list load [file join $dir tkimgpng13.dll]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::ppm" 1.3 [list load [file join $dir tkimgppm13.dll]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::ps" 1.3 [list load [file join $dir tkimgps13.dll]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::sgi" 1.3 [list load [file join $dir tkimgsgi13.dll]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::sun" 1.3 [list load [file join $dir tkimgsun13.dll]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::tga" 1.3 [list load [file join $dir tkimgtga13.dll]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::tiff" 1.3 [list load [file join $dir tkimgtiff13.dll]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::window" 1.3 [list load [file join $dir tkimgwindow13.dll]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::xbm" 1.3 [list load [file join $dir tkimgxbm13.dll]]
-# Tcl package index file - handcrafted
-#
-# $Id: pkgIndex.tcl,v 1.3 2005-11-23 12:54:00 matben Exp $
-
-package ifneeded "img::xpm" 1.3 [list load [file join $dir tkimgxpm13.dll]]
+package ifneeded img::bmp 1.3  [list load [file join $dir tkimgbmp13.dll]]
+package ifneeded img::gif 1.3  [list load [file join $dir tkimggif13.dll]]
+package ifneeded img::ico 1.3  [list load [file join $dir tkimgico13.dll]]
+package ifneeded img::jpeg 1.3  [list load [file join $dir tkimgjpeg13.dll]]
+package ifneeded img::pcx 1.3  [list load [file join $dir tkimgpcx13.dll]]
+package ifneeded img::pixmap 1.3  [list load [file join $dir tkimgpixmap13.dll]]
+package ifneeded img::png 1.3  [list load [file join $dir tkimgpng13.dll]]
+package ifneeded img::ppm 1.3  [list load [file join $dir tkimgppm13.dll]]
+package ifneeded img::ps 1.3  [list load [file join $dir tkimgps13.dll]]
+package ifneeded img::sgi 1.3  [list load [file join $dir tkimgsgi13.dll]]
+package ifneeded img::sun 1.3  [list load [file join $dir tkimgsun13.dll]]
+package ifneeded img::tga 1.3  [list load [file join $dir tkimgtga13.dll]]
+package ifneeded img::tiff 1.3  [list load [file join $dir tkimgtiff13.dll]]
+package ifneeded img::window 1.3  [list load [file join $dir tkimgwindow13.dll]]
+package ifneeded img::xbm 1.3  [list load [file join $dir tkimgxbm13.dll]]
+package ifneeded img::xpm 1.3  [list load [file join $dir tkimgxpm13.dll]]
