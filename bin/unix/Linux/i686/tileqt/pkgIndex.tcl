40df8779b4f8d959321f066e816e0542ab9e5a55
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2006-05-26 14:19:08 +0000

    * Added tileqt.

---
+++ bin/unix/Linux/i686/tileqt/pkgIndex.tcl
@@ -0,0 +1,8 @@
+#
+# pkgIndex.tcl.  Generated from pkgIndex.tcl.in by configure.
+#
+#package ifneeded tile::theme::tileqt 0.4 [subst {
+#    load [file join $dir libtileqt0.4.so] tileqt
+#}]
+
+package tile::theme::tileqt 0.4  [list load [file join $dir libtileqt0.4.so] tileqt]
