dd6467cdf269628ab55452b9697a456dfde3b5ae
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2006-05-26 13:26:54 +0000

    * Removed comments in cmpntIndex file.

--- trunk/coccinella/components/Phone/cmpntIndex.tcl
+++ trunk/coccinella/components/Phone/cmpntIndex.tcl
@@ -1,6 +1,6 @@
 # See contrib/component.tcl for explanations.
 #
 
-#component::attempt AddressBook [file join $dir AddressBook.tcl] ::AddressBook::Init
-#component::attempt Phone       [file join $dir Phone.tcl]       ::Phone::Init
+component::attempt AddressBook [file join $dir AddressBook.tcl] ::AddressBook::Init
+component::attempt Phone       [file join $dir Phone.tcl]       ::Phone::Init
 
