0a9a83e37caff07159952831054066bd9a617d40
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2006-08-01 06:18:33 +0000

    * More code by Antonio. Using PEP JEP.

--- trunk/coccinella/components/Mood.tcl
+++ trunk/coccinella/components/Mood.tcl
@@ -17,100 +17,78 @@ proc ::Mood::Init { } {
     ::hooks::register loginHook             ::Mood::LoginHook
 
     variable server
-    variable psjid
     variable myjid
     variable moodjlib
 
-    variable nodeUser
-    variable nodeMood
+    variable node
+    set node "http://jabber.org/protocol/moodee"
 
     variable xmlnsMood
     set xmlnsMood "http://jabber.org/protocol/mood"
 
     variable state
+
+    variable menuMood
+    set menuMood [list  \
+      command mMood [namespace current]::Cmd  normal {}]
 }
 
 proc ::Mood::LoginHook { } {
     variable server
-    variable psjid
     variable myjid
     variable moodjlib
-    variable nodeUser
-    variable nodeMood
- 
-    variable xmlnsMood
-
-    set menuMood [list  \
-      command mMood [namespace current]::Cmd  normal {}]
 
 #    lappend menuMood [list radio angry [namespace current]::Cmd normal {} {}]
 #    lappend menuMood [list radio bored [namespace current]::Cmd normal {} {}]
-
-    ::Jabber::UI::RegisterMenuEntry jabber $menuMood
 
     #----- Initialize variables ------
     set moodjlib jlib::jlib1
     set myjid [$moodjlib myjid2]
     set server [$moodjlib getserver]
-    set psjid pubsub.$server
 
-    set nodeUser /home/$server/antoniofcano
-    set nodeMood $nodeUser/mood
-
-    #Disco server for pubsub support
+    #----- Disco server for pubsub support -----
     ::Jabber::JlibCmd disco get_async items $server ::Mood::OnDiscoServer
 }
 
 proc ::Mood::OnDiscoServer {jlibname type from subiq args} {
     variable state
-    variable psjid
     variable myjid
     variable moodjlib
-    variable nodeUser
-    variable nodeMood
+    variable node
     variable xmlnsMood
+    variable menuMood
  
     ::Debug 2 "::Mood::OnDiscoServer"
-        
+
     if {$type eq "result"} {
         set childs [::Jabber::JlibCmd disco children $from]
         foreach service $childs {
             set name [::Jabber::JlibCmd disco name $service]
 
             ::Debug 2 "\t service=$service, name=$name"
-#           @@@ $name equal null ??
+#           @@@ Has to look for identity=pep 
 #           if {$name eq "pubsub"} {
             if { [string first "pubsub" $service] != -1 } {
-                # Caps specific Mood stuff.
-                set subtags [list [wrapper::createtag "identity"  \
-                  -attrlist [list category hierarchy type leaf name "User Mood"]]]
-                lappend subtags [wrapper::createtag "feature" \
-                  -attrlist [list var $xmlnsMood]]
-                ::Jabber::RegisterCapsExtKey mood $subtags
+                #------ Initialize User Interface -------
+                ::Jabber::UI::RegisterMenuEntry jabber $menuMood
 
-                #---- Checks if node User exists in server ----
-                $moodjlib pubsub items $psjid $nodeUser -command ::Mood::PubSubItemUserCB
-
-                #---- Register to PubSub service
-                #$moodjlib pubsub subscribe $psjid $myjid -node $nodeMood -command ::Mood::PubSubCB
-                #$moodjlib pubsub register_event ::Mood::PubSubCBEvent -from $psjid -node $nodeMood
-
+                #----- Create Node for Mood -------------
+                ::Jabber::JlibCmd disco get_async items $myjid [list ::Mood::CreateNode]
                 break
             }
 #            }
         }
     }
 }
-proc ::Mood::Cmd {{mood ""} {text ""}} {    
+proc ::Mood::Cmd {{moodState ""} {text ""}} {    
     variable server
-    variable psjid
     variable myjid
     variable moodjlib
-    variable nodeMood
+    variable node
     variable xmlnsMood
 
     #@@@ This comes from UI
-    set mood "happy"
+    set moodState "happy"
     set text "nobody"
 
     set moodValues [list afraid amazed angry annoyed anxious aroused ashamed bored brave calm cold confused contented cranky \
@@ -119,78 +97,66 @@ proc ::Mood::Cmd {{mood ""} {text ""}} {
                          invincible jealous lonely mean moody nervous neutral offended playful proud relieved remorseful restless \
                          sad sarcastic serious shocked shy sick sleepy stressed surprised thirsty worried]
 
-    if {[lsearch $moodValues $mood] >= 0} {
+    if {[lsearch $moodValues $moodState] >= 0} {
 	set moodTextTag [wrapper::createtag text -chdata $text] 
 
-        set moodStateTag [wrapper::createtag $mood]
+        set moodStateTag [wrapper::createtag $moodState]
         set moodTag [wrapper::createtag mood -attrlist [list xmlns $xmlnsMood] -subtags [list  $moodStateTag $moodTextTag] ]
 
-        set itemElem [wrapper::createtag item -attrlist [list id current] -subtags [list $moodTag]]
+        set itemElem [wrapper::createtag item -subtags [list $moodTag]]
 
-        $moodjlib pubsub publish $psjid $nodeMood  -items [list $itemElem]
+        $moodjlib pubsub publish $node  -items [list $itemElem] -command ::Mood::PubSubPublishCB
     }
 }
 
-proc ::Mood::PubSubCB {type args} { 
-	puts "@@@@@ (PubSub) ---> $args"
-}
-
-proc ::Mood::PubSubCBEvent {args} {
-     puts "@@@@@ (Event) ----> $args"
+proc ::Mood::PubSubPublishCB {args} {
+     puts "(Publish Results)----> $args"
 }
 
 #--------------------------------------------------------------------
 #-------             Checks and create if node exists ---------------
 #-----------            before the publish tasks     ----------------
 #--------------------------------------------------------------------
-proc ::Mood::PubSubItemUserCB {type args} {
+proc ::Mood::CreateNode {jlibname type from subiq args} {
 variable server
-variable psjid
 variable myjid
 variable moodjlib
-variable nodeUser
-variable nodeMood
+variable node
 
-    ### Not Supported by Ejabberd -> $moodjlib pubsub delete $psjid $nodeMood 
-
-    #@@@@ control that error is item-not-found
-    if {$type eq "error"} {
-	#Create User Node
-        $moodjlib pubsub create $psjid -node $nodeUser -command ::Mood::PubSubCreateCB
+    #------- Before create a node checks if it is created ---------
+    set findMood false
+    if {$type eq "result"} {
+        set nodes [::Jabber::JlibCmd disco nodes $from]
+        foreach nodeItem $nodes {
+            if { [string first "moodee" $nodeItem] != -1 } {
+                set findMood true
+                break
+            }
+        }
     }
 
-    #Check for Mood node
-    $moodjlib pubsub items $psjid $nodeMood -command ::Mood::PubSubItemMoodCB
-}
+    #---------- Create the node for mood information -------------
+    if { !$findMood } {
+        #Configure setup for PEP node
+        set valueFormElem  [wrapper::createtag value -chdata "http://jabber.org/protocol/pubsub#node_config"]
+        set fieldFormElem [wrapper::createtag field -attrlist [list var "FORM_TYPE" type hidden] -subtags [list $valueFormElem]]
 
-proc ::Mood::PubSubItemMoodCB {type args} {
-variable server
-variable psjid
-variable myjid
-variable moodjlib
-variable nodeMood
+        #PEP Values for access_model: roster / presence / open or authorize / whitelist
+        set valueAccessModel [wrapper::createtag value -chdata roster]
+        set fieldAccessModelElem [wrapper::createtag field -attrlist [list var "pubsub#access_model"] -subtags [list $valueAccessModel]]
 
-        #Configure setup for open access
-        set valueAccessModel [wrapper::createtag value -chdata open]
-        #@@@ In Jep Version 1.8 the field name is: access_model
-        set fieldAccessModelElem [wrapper::createtag field -attrlist [list var "pubsub#subscription_model"] -subtags [list $valueAccessModel]]
-        set xElem [wrapper::createtag x -attrlist [list xmlns "jabber:x:data" type submit] -subtags [list $fieldAccessModelElem]]
+        set xElem [wrapper::createtag x -attrlist [list xmlns "jabber:x:data" type submit] -subtags [list $fieldFormElem $fieldAccessModelElem]]
 
-        $moodjlib pubsub configure set $psjid $nodeMood -x $xElem -command ::Mood::PubSubOptions
-#        $moodjlib pubsub configure get $psjid $nodeMood -command ::Mood::PubSubOptions
-
-    if {$type eq "error"} {
-        $moodjlib pubsub create $psjid -node $nodeMood -command ::Mood::PubSubCreateCB -configure $xElem
+        $moodjlib pubsub create -node $node -command ::Mood::PubSubCreateCB -configure $xElem
     }
 }
 
-proc ::Mood::PubSubOptions {args} {
-    puts "(Configure)----> $args"
-}
 proc ::Mood::PubSubCreateCB {type args} {
-        puts "---> $args"
-}
 
+    if { $type ne 'result' } {
+        puts "(CreateNode error)----> $args"
+    }
+}
 
 #-------------------------------------------------------------------------
 #---------------------- (Extended Presence) ------------------------------
@@ -221,45 +187,38 @@ proc ::Mood::PresenceHook {jid type args} {
     jlib::splitjidex $from node jidserver res
     set jid2 $node@$jidserver
 
-    if { ![info exists state($jidserver,pubsubsupport)] } {
-        ::Jabber::JlibCmd disco get_async items $jidserver [list ::Mood::OnDiscoContactServer $node]
-    } else {
-        if { $state($jidserver,pubsubsupport) eq true } {
-            set nodeMood /home/$jidserver/$node/mood
-            $moodjlib pubsub subscribe $state($jidserver,pubsubservice) $jid2 -node $nodeMood -command ::Mood::PubSubCB
-        }
-    }    
+    if { ![info exists state($from,pubsubsupport)] } {
+        #------------- Check(disco#items) If the User supports PEP before subscribe
+        ::Jabber::JlibCmd disco get_async items $jid2 [list ::Mood::OnDiscoContact]
+    } 
 }
 
-#-------------------------------------------------------------------------
-#----------------------------- (Disco) -----------------------------------
-#-------------------------------------------------------------------------
-proc ::Mood::OnDiscoContactServer {node jlibname type from subiq args} {
+proc ::Mood::OnDiscoContact {jlibname type from subiq args} {
     variable state
     variable moodjlib
+    variable myjid
+    variable node
 
     ::Debug 2 "::Mood::OnDiscoContactServer"
 
+    # --- Check if contact supports Mood node ----
     if {$type eq "result"} {
-        set childs [::Jabber::JlibCmd disco children $from]
-        foreach service $childs {
-            set name [::Jabber::JlibCmd disco name $service]
-
-            ::Debug 2 "\t service=$service, name=$name"
-#           @@@ $name equal null ??
-#           if {$name eq "pubsub"} {
-            if { [string first "pubsub" $service] != -1 } {
+        set nodes [::Jabber::JlibCmd disco nodes $from]
+        foreach nodeItem $nodes {
+            if { [string first "mood" $nodeItem] != -1 } {
                 set state($from,pubsubsupport) true
-                set state($from,pubsubservice) $service
-            
-                set jid2 $node@$from
-                set nodeMood /home/$from/$node/mood
-                $moodjlib pubsub subscribe $service $jid2 -node $nodeMood -command ::Mood::PubSubCB
+           
+                jlib::splitjidex $from nodeFrom jidserver res
+                set jid2 $nodeFrom@$jidserver 
+                $moodjlib pubsub subscribe $jid2 $myjid -node $node -command ::Mood::PubSubscribeCB
                 break
             }
-#           }
         }
     }
+}
+
+proc ::Mood::PubSubscribeCB {type args} {
+        puts "@@@@@ (PubSub) ---> $args"
 }
 
 #-------------------------------------------------------------------------
@@ -294,10 +253,9 @@ proc ::Mood::MessageHook {body args} {
         set text [wrapper::getcdata $textElem]
     }
 
+   #@@@ Add Mood info into user ballon
+   #???
     puts "Node: $node"
     puts "Text: $text"
     puts "Mood: $mood"
-
-   #@@@ Add Mood info into user ballon
-   #???
 }
