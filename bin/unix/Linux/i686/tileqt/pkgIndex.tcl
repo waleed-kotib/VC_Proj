0bf2b08af33117b99fa0e64d08f7427b7bdba678
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2006-09-04 12:07:26 +0000

    * Protect from loading on tclkit earlier than 8.4.13

--- bin/unix/Linux/i686/tileqt/pkgIndex.tcl
+++ bin/unix/Linux/i686/tileqt/pkgIndex.tcl
@@ -5,4 +5,12 @@
 #    load [file join $dir libtileqt0.4.so] tileqt
 #}]
 
+# tileqt crashes on tclkits 8.4.11 and older. Only my own build?
+
+if {[lindex [file system [info library]] 0] ne "native"} {
+    if {[package vcompare [info patchlevel] 8.4.11] <= 0} {
+	puts stderr "Warning: the tileqt package rejected loading on tclkit 8.4.11 or earlier"
+	return
+    }   
+}
 package ifneeded tile::theme::tileqt 0.4  [list load [file join $dir libtileqt0.4.so] tileqt]
