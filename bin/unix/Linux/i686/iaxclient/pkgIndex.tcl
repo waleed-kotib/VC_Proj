70be12f5f597a21f12a4e7482f3cea4d5f9caaa5
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2006-11-08 08:33:36 +0000

    * Added iaxclient 0.2 build with ALSA and SVN head libiaxclient.

--- bin/unix/Linux/i686/iaxclient/pkgIndex.tcl
+++ bin/unix/Linux/i686/iaxclient/pkgIndex.tcl
@@ -4,8 +4,8 @@
 if {[file exists /dev/dsp] && [file writable /dev/dsp]} {
     if {![catch {open /dev/dsp "WRONLY NONBLOCK"} f]} {
 	close $f
-	package ifneeded iaxclient 0.1 \
-	  "[list load [file join $dir libiaxclient0.1.so] iaxclient]; \
+	package ifneeded iaxclient 0.2 \
+	  "[list load [file join $dir libiaxclient0.2.so] iaxclient]; \
 	  [list source [file join $dir iaxclient.tcl]]"
     }
 }
