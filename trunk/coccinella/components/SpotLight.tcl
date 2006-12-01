32d070106074a3ab677d66c38fdabd7b83e3bee5
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2006-12-01 08:57:49 +0000

    * Updated Antonios Spotlight +Gdesktop.

--- trunk/coccinella/components/SpotLight.tcl
+++ trunk/coccinella/components/SpotLight.tcl
@@ -4,32 +4,55 @@
 #       
 #  Copyright (c) 2006 Antonio Cano Damas
 #
-# $Id: SpotLight.tcl,v 1.2 2006-11-24 08:01:54 matben Exp $
+# $Id: SpotLight.tcl,v 1.3 2006-12-01 08:57:49 matben Exp $
 
 namespace eval ::SpotLight:: { }
 
 proc ::SpotLight::Init { } {
-    global  this
+    global this
+    global tcl_platform
 
+
+    #---- Check support of Indexer into our Operating System -------
     if {![string equal $this(platform) "macosx"]} {
-        return
+        if {![string equal $this(platform) "windows"]} {
+            #@@@ Support for Beagle in Linux coming...
+            return
+        } else {
+            #Get Google Desktop Registry Entry
+		if {[catch {package require registry}]} {
+                return
+            } else {
+                variable gDesktopURL
+                set gDesktopURL [registry get "HKEY_CURRENT_USER\\Software\\Google\\Google Desktop\\API" "search_url"]
+                #If Google Desktop is Not installed return
+                if {$gDesktopURL eq ""} {
+                    return
+                }
+            }
+        }
+    } else {
+	#Check that we are running over a Tiger or greater version of OSX
+        set darwinVersion [string index $tcl_platform(osVersion) 0] 
+        if { $darwinVersion < 8 } {
+		return
+        } else {
+            #Load AppleScript support needed for launching SpotLight
+            if {[catch {package require Tclapplescript}]} {
+                return
+            }
+        }
     }
 
-    if {[catch {package require Tclapplescript}]} {
-        return
-    }
-    
-    # Must check that spotlight exists on this machine.
-    return
-    
     # Add event hooks.
-    ::hooks::register newChatMessageHook ::SpotLight::ChatMessageHook
+    ::hooks::register newChatMessageHook ::SpotLight::ChatMessageHook 1
 
-    component::register SpotLight \
-      {Launch SpotLight with incoming messages.}
+    component::register SpotLight {Launch SpotLight with incoming messages.}
 }
 
 proc ::SpotLight::ChatMessageHook {body args} {
+    global this
+
     if {$body eq ""} {
         return
     }
@@ -37,11 +60,11 @@ proc ::SpotLight::ChatMessageHook {body args} {
 
     # -from is a 3-tier jid /resource included.
     set jid $argsA(-from)
-    jlib::splitjid $jid jid2 -
+    set jid2 [jlib::barejid $jid]
 
-#    if {[::Chat::HaveChat $jid]} {
-#        return
-#    }
+    if {[::Chat::HaveChat $jid]} {
+        return
+    }
 
     set search_string "$jid2"
 
@@ -49,32 +72,55 @@ proc ::SpotLight::ChatMessageHook {body args} {
         append search_string " $argsA(-subject)"
     }
 
-    ::SpotLight::Launch $search_string
+    if {[string equal $this(platform) "macosx"]} {
+        LaunchSL $search_string
+    } else {
+        LaunchGD $search_string
+    }
 }
 
-proc ::SpotLight::Launch {search_string} {
-	set script " 
-		--Search spotlight key shortcuts \n
-		set pList to (path to preferences folder as string) & \"com.apple.universalaccess.plist\" \n
-		tell application \"System Events\" \n
-			set pList to property list file pList \n
-			set pListItems to property list items of property list item \"UserAssignableHotKeys\" of contents of pList \n
-			set theItem to 1st item of pListItems whose value of property list item 4 is 65 -- index of spotlight shortcut (or 64 for spotlight window) \n
-			set {theKey, keyEnabled, ModifierKeys, idx} to value of property list items of theItem \n
-			if (keyEnabled = 1) or (keyEnabled = true) then \n
-				set ModifierKeys to ModifierKeys / 131072 as integer -- strip bits 0 - 16 \n
-				--Press shortcuts \n
-				if ModifierKeys div 8 mod 2 = 1 then key down command \n
-				if ModifierKeys div 4 mod 2 = 1 then key down option \n
-				if ModifierKeys div 2 mod 2 = 1 then key down control \n
-				if ModifierKeys mod 2 = 1 then key down shift \n
-				key code theKey \n
-				key up {command, option, control, shift} \n
-				--Send to spotlight query text \n
-				delay 1 \n
-				keystroke \"$search_string\" \n
-			end if \n
-		end tell"
-	AppleScript execute $script
+#-------------------------------------------
+#     Query Launchers
+#-------------------------------------------
+
+#-- Google Desktop for Windows
+proc ::SpotLight::LaunchGD {search_string} {
+    variable gDesktopURL
+
+    set launchURL "$gDesktopURL$search_string"
+
+    ::Utils::OpenURLInBrowser $launchURL
+}
+
+#-- SpotLight for Mac OSX Tiger
+proc ::SpotLight::LaunchSL {search_string} {
+    global  this
+    
+    if {$this(package,Tclapplescript)} {
+	set script {
+		--Search spotlight key shortcuts
+		set pList to (path to preferences folder as string) & "com.apple.universalaccess.plist"
+		tell application "System Events"
+			set pList to property list file pList
+			set pListItems to property list items of property list item "UserAssignableHotKeys" of contents of pList
+			set theItem to 1st item of pListItems whose value of property list item 4 is 65 -- index of spotlight shortcut (or 64 for spotlight window)
+			set {theKey, keyEnabled, ModifierKeys, idx} to value of property list items of theItem
+			if (keyEnabled = 1) or (keyEnabled = true) then
+				set ModifierKeys to ModifierKeys / 131072 as integer -- strip bits 0 - 16
+				--Press shortcuts
+				if ModifierKeys div 8 mod 2 = 1 then key down command
+				if ModifierKeys div 4 mod 2 = 1 then key down option
+				if ModifierKeys div 2 mod 2 = 1 then key down control
+				if ModifierKeys mod 2 = 1 then key down shift
+				key code theKey
+				key up {command, option, control, shift}
+				--Send to spotlight query text
+				delay 1
+				keystroke "%s"
+			end if
+		end tell
+	}
+	AppleScript execute [format $script $search_string]
+    }
 }
 
