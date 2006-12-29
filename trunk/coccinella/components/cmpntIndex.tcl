f04b0584635df64889462b7f63492886bc4162b7
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2006-12-29 14:08:16 +0000

    * Switched off Mood since untested.

--- trunk/coccinella/components/cmpntIndex.tcl
+++ trunk/coccinella/components/cmpntIndex.tcl
@@ -4,13 +4,12 @@
 component::attempt AppleEvents  [file join $dir AppleEvents.tcl] ::AppleEvents::Init
 component::attempt AutoUpdate   [file join $dir AutoUpdate.tcl]  ::AutoUpdate::Init
 component::attempt BuddyPounce  [file join $dir BuddyPounce.tcl] ::BuddyPounce::Init
-component::attempt TtkDialog    [file join $dir TtkDialog.tcl]   ::TtkDialog::Init
 component::attempt GnomeMeeting [file join $dir GMeeting.tcl]    ::GMeeting::Init
 component::attempt ICQ          [file join $dir ICQ.tcl]         ::ICQ::Init
 component::attempt ImageMagic   [file join $dir ImageMagic.tcl]  ::ImageMagic::Init
 component::attempt JivePhone    [file join $dir JivePhone.tcl]   ::JivePhone::Init
 component::attempt MailtoURI    [file join $dir MailtoURI.tcl]   ::MailtoURI::Init
-component::attempt Mood         [file join $dir Mood.tcl]        ::Mood::Init
+#component::attempt Mood         [file join $dir Mood.tcl]        ::Mood::Init
 component::attempt Notifier     [file join $dir Notifier.tcl]    ::Notifier::Init
 component::attempt NotifyOnline [file join $dir NotifyOnline.tcl] ::NotifyOnline::Init
 component::attempt ParseMeCData [file join $dir ParseMeCData.tcl] ::ParseMeCData::Init
@@ -20,6 +19,7 @@ component::attempt SlideShow    [file join $dir SlideShow.tcl]   ::SlideShow::Lo
 component::attempt Sounds       [file join $dir Sounds.tcl]      ::Sounds::Load    
 component::attempt Speech       [file join $dir Speech.tcl]      ::Speech::Load    
 component::attempt SpotLight    [file join $dir SpotLight.tcl]   ::SpotLight::Init    
+component::attempt TtkDialog    [file join $dir TtkDialog.tcl]   ::TtkDialog::Init
 component::attempt URIRegistry  [file join $dir URIRegistry.tcl] ::URIRegistry::Init
 component::attempt WhiteboardMK [file join $dir WhiteboardMK.tcl]  ::WhiteboardMK::Init
 
