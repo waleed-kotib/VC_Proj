b8a7425b7abcf3210523e9439b3236b39712b41c
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2007-03-14 15:03:18 +0000

    * Updet treectrl: mac build now 2.2.3 and win 2.2.3; linux to do.

--- bin/windows/intel/treectrl/pkgIndex.tcl
+++ bin/windows/intel/treectrl/pkgIndex.tcl
@@ -1,3 +1,3 @@
 if {[catch {package require Tcl 8.4}]} return
 
-package ifneeded treectrl 2.1 [list load [file join $dir treectrl21.dll] treectrl]
+package ifneeded treectrl 2.2 [list load [file join $dir treectrl22.dll] treectrl]
