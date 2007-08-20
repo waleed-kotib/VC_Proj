0ab8409a39e4d1022965e868e2cf7b26dccef41d
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2007-08-20 14:07:52 +0000

    * Added pixmap theme WinXP-Blue

---
+++ bin/library/WinXP-Blue/pkgIndex.tcl
@@ -0,0 +1,15 @@
+# pkgIndex.tcl for additional tile pixmap themes.
+#
+# We don't provide the package is the image subdirectory isn't present,
+# or we don't have the right version of Tcl/Tk
+#
+# To use this automatically within tile, the tile-using application should
+# use tile::availableThemes and tile::setTheme 
+#
+# $Id: pkgIndex.tcl,v 1.1 2007-08-20 14:07:52 matben Exp $
+
+if {![file isdirectory [file join $dir WinXP-Blue]]} { return }
+if {![package vsatisfies [package provide Tcl] 8.4]} { return }
+
+package ifneeded tile::theme::winxpblue 0.5 \
+    [list source [file join $dir WinXP-Blue.tcl]]
