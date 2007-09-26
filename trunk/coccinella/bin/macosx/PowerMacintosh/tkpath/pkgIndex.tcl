9fc073d75505a2411f17c4668e41b26eb32c5b39
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2007-09-26 12:03:57 +0000

    * New version 0.2.8 which adds -fill for polyline and bugfix for coords pimage.

--- trunk/coccinella/bin/macosx/PowerMacintosh/tkpath/pkgIndex.tcl
+++ trunk/coccinella/bin/macosx/PowerMacintosh/tkpath/pkgIndex.tcl
@@ -1,5 +1,5 @@
 #
 #  We need both dynaimc lib and script support files.
 
-package ifneeded tkpath 0.2.6 "[list load [file join $dir tkpath0.2.6.dylib]]; \
+package ifneeded tkpath 0.2.8 "[list load [file join $dir tkpath0.2.8.dylib]]; \
     [list source [file join $dir tkpath.tcl]]"
