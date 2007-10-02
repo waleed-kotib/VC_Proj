a5b0ad8f7aae3a78b4dad6a8a4e02aa686153c29
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2007-10-02 06:39:13 +0000

    * More code...

--- jabberlib/sipub.tcl
+++ jabberlib/sipub.tcl
@@ -7,7 +7,7 @@
 #  
 # This file is distributed under BSD style license.
 #  
-# $Id: sipub.tcl,v 1.1 2007-09-30 08:37:24 matben Exp $
+# $Id: sipub.tcl,v 1.2 2007-10-02 06:39:13 matben Exp $
 # 
 
 package require jlib		
@@ -22,11 +22,16 @@ namespace eval jlib::sipub {
     set xmlns(sipub) "http://jabber.org/protocol/si/profile/sipub"
 	        
     jlib::disco::registerfeature $xmlns(sipub)
+    $jlibname iq_register get $xmlns(sipub) [namespace current]::handle_get
 
+    # We use a static cache array that maps sipub id (spid) to file name and mime.
+    variable cache
+    
     # Note: jlib::ensamble_register is last in this file!
 }
 
 proc jlib::sipub::init {jlibname args} {
+    variable cache
 
     
 }
@@ -35,8 +40,77 @@ proc jlib::sipub::cmdproc {jlibname cmd args} {
     return [eval {$cmd $jlibname} $args]
 }
 
+# jlib::sipub::set_cache, get_cache --
+# 
+#       Set or get the complete cache. Useful if we store the cache in a file
+#       between sessions.
 
+proc jlib::sipub::set_cache {cacheL} {
+    variable cache
+    array set cache cacheL
+}
 
+proc jlib::sipub::get_cache {} {
+    variable cache
+    return [array get cache]
+}
+
+# jlib::sipub::element --
+# 
+#       Makes a sipub element for a local file and adds the reference to cache.
+
+proc jlib::sipub::element {jlibname fileName mime} {
+    variable xmlns
+    variable cache
+
+    set spid [jlib::generateuuid]
+    set cache($spid,file) $fileName
+    set cache($spid,mime) $mime
+    
+    # FIX!
+    set xE [wrapper::createtag "x"              \
+      -attrlist {xmlns jabber:x:data type form} -subtags [list $fieldE]]
+    set featureE [wrapper::createtag "feature"  \
+      -attrlist [list xmlns $xmlns(neg)] -subtags [list $xE]]
+    set sipubE [wrapper::createtag "sipub"  \
+      -attrlist [list xmlns $xmlns(sipib) id $spid mime-type $mime profile $profile] \
+      -subtags [list $profileE $featureE]]
+
+    return $sipubE
+}
+
+proc jlib::sipub::send_get {jlibname } {
+
+    
+}
+
+# jlib::sipub::handle_get --
+# 
+#       Handles incoming iq-get/sipub stanzas.
+
+proc jlib::sipub::handle_get {jlibname from iqChild args} {
+    variable cache
+
+    array set argsA $args
+    array set attr [wrapper::getattrlist $iqChild]
+    if {![info exists argsA(-id)]} {
+	return 0
+    }
+    set id $argsA(-id)
+    if {![info exists attr(id)]} {
+	return 0
+    }
+    set spid $attr(id)    
+    if {[info exists cache($spid.file)]} {
+	
+	# ???
+	$jlibname filetransfer send $from $send_cb -file $cache($spid,file) \
+	  -mime $cache($spid,mime) -progress
+    } else {
+	jlib::send_iq_error $jlibname $from $id 403 cancel forbidden
+    }
+    return 1
+}
 
 # We have to do it here since need the initProc before doing this.
 
