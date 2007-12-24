f761c3d5b69b5ce4bc1e6699026e5c63eb45adbe
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2007-12-24 09:32:00 +0000

    * Forgot to switch off debugging.

--- Coccinella.tcl
+++ Coccinella.tcl
@@ -12,10 +12,10 @@
 #  
 #  See the README file for license, bugs etc.
 #
-# $Id: Coccinella.tcl,v 1.172 2007-12-24 09:31:14 matben Exp $	
+# $Id: Coccinella.tcl,v 1.173 2007-12-24 09:32:00 matben Exp $	
 
 # Level of detail for printouts; >= 2 for my outputs; >= 6 to logfile.
-set debugLevel 4
+set debugLevel 0
 
 # TclKit loading mechanism.
 package provide app-Coccinella 1.0
