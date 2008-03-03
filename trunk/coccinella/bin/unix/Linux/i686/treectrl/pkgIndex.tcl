a20301f4499bd4b95c0f2c6eca7a5d1afa4f874b
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2008-03-03 08:35:58 +0000

    * Cleaned up old 2.1 build.  * Added fresh 2.2.6 build in its own dir since it seems require 8.5.

--- trunk/coccinella/bin/unix/Linux/i686/treectrl/pkgIndex.tcl
+++ trunk/coccinella/bin/unix/Linux/i686/treectrl/pkgIndex.tcl
@@ -1,3 +1,7 @@
 if {[catch {package require Tcl 8.4}]} return
 
-package ifneeded treectrl 2.2.3 [list load [file join $dir libtreectrl2.2.so] treectrl]
+if {[info tclversion] == 8.4} {
+    package ifneeded treectrl 2.2.3 [list load [file join $dir libtreectrl2.2.so] treectrl]
+} elseif {[info tclversion] == 8.5} {
+    package ifneeded treectrl 2.2.6 [list load [file join $dir 8.5 libtreectrl2.2.so] treectrl]    
+}
