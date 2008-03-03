d8655309fca7d6632669ef3fe41ff8f27668c74a
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2008-03-03 08:17:29 +0000

    * Cleand up old 2.1 dll.  * Added MinGW built using 8.5. Seems that it requires 8.5 why I put it in separate dir.    Not sure why it got so big.

--- trunk/coccinella/bin/windows/intel/treectrl/pkgIndex.tcl
+++ trunk/coccinella/bin/windows/intel/treectrl/pkgIndex.tcl
@@ -1,3 +1,6 @@
 if {[catch {package require Tcl 8.4}]} return
-
-package ifneeded treectrl 2.2.3 [list load [file join $dir treectrl22.dll] treectrl]
+if {[info tclversion] == 8.4} {
+    package ifneeded treectrl 2.2.3 [list load [file join $dir treectrl22.dll] treectrl]
+} elseif {[info tclversion] == 8.5} {
+    package ifneeded treectrl 2.2.6 [list load [file join $dir 8.5 treectrl22.dll] treectrl]    
+}
