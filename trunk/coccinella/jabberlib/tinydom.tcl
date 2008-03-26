f9429197edb1a9ce784e91d09ee6994fc56ca722
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2008-03-26 13:11:34 +0000

    * Rewrote the tinydom parser to be more OO to avoid any potential reentrance problems.

--- trunk/coccinella/jabberlib/tinydom.tcl
+++ trunk/coccinella/jabberlib/tinydom.tcl
@@ -1,32 +1,28 @@
 #  tinydom.tcl ---
 #  
 #      This file is part of The Coccinella application. It implements 
-#      a tiny DOM model which wraps xml int tcl lists.
+#      a tiny DOM model which wraps xml into tcl lists.
 #
 #  Copyright (c) 2003  Mats Bengtsson
 #  
 # This file is distributed under BSD style license.
 #  
-# $Id: tinydom.tcl,v 1.13 2007-07-19 06:28:18 matben Exp $
+# $Id: tinydom.tcl,v 1.14 2008-03-26 13:11:34 matben Exp $
 
 package require xml
 
-package provide tinydom 0.1
+package provide tinydom 0.2
 
 # This is an attempt to make a minimal DOM thing to store xml data as
 # a hierarchical list which is better suited to Tcl.
 # @@@ Try make a common syntax with wrapper.
 
 namespace eval tinydom {
-
-    variable xmlobj
-    variable level 0
     variable uid 0
     variable cache
 }
 
 proc tinydom::parse {xml args} {
-    variable xmlobj
     variable uid
     variable cache
 
@@ -46,61 +42,66 @@ proc tinydom::parse {xml args} {
 	    return -code error "unknown -package \"$argsA(-package)\""
 	}
     }
-    $xmlparser configure -reportempty 1   \
-      -elementstartcommand  [namespace current]::XmlElementStart     \
-      -elementendcommand    [namespace current]::XmlElementEnd       \
-      -characterdatacommand [namespace current]::XmlCHdata           \
-      -ignorewhitespace     1
-    $xmlparser parse $xml
-    
+
     # Store in internal array and return token which is the array index.
     set token [namespace current]::[incr uid]
-    set cache($token) $xmlobj(1)
-    unset xmlobj
+    upvar #0 $token state
+    
+    set state(1) [list]
+    set state(level) 0
+    
+    $xmlparser configure -reportempty 1   \
+      -elementstartcommand  [namespace code [list ElementStart $token]] \
+      -elementendcommand    [namespace code [list ElementEnd $token]]   \
+      -characterdatacommand [namespace code [list CHdata $token]]       \
+      -ignorewhitespace     1
+    $xmlparser parse $xml
+
+    set cache($token) $state(1)
+    unset state
     return $token
 }
 
-proc tinydom::XmlElementStart {tagname attrlist args} {
-    variable xmlobj
-    variable level
+proc tinydom::ElementStart {token tag attrlist args} {
+    upvar #0 $token state
 
     array set argsA $args
     if {[info exists argsA(-namespacedecls)]} {
 	lappend attrlist xmlns [lindex $argsA(-namespacedecls) 0]
     }
-    set xmlobj([incr level]) [list $tagname $attrlist 0 {} {}]
+    set state([incr state(level)]) [list $tag $attrlist 0 {} {}]
 }
 
-proc tinydom::XmlElementEnd {tagname args} {
-    variable xmlobj
-    variable level
+proc tinydom::ElementEnd {token tagname args} {
+    upvar #0 $token state
 
+    set level $state(level)
     if {$level > 1} {
     
 	# Insert the child tree in the parent tree.
-	XmlAppend [expr $level-1] $xmlobj($level)
+	Append $token [expr $level-1] $state($level)
     }
-    incr level -1
+    incr state(level) -1
 }
 
-proc tinydom::XmlCHdata {chdata} {
-    variable xmlobj
-    variable level
+proc tinydom::CHdata {token chdata} {
+    upvar #0 $token state
 
-    set cdata [lindex $xmlobj($level) 3]
+    set level $state(level)
+    set cdata [lindex $state($level) 3]
     append cdata [xmldecrypt $chdata]
-    lset xmlobj($level) 3 $cdata
+    lset state($level) 3 $cdata
 }
 
-proc tinydom::XmlAppend {plevel childtree} {
-    variable xmlobj
+proc tinydom::Append {token plevel childtree} {
+    upvar #0 $token state
     
     # Get child list at parent level (level).
-    set childlist [lindex $xmlobj($plevel) 4]
+    set childlist [lindex $state($plevel) 4]
     lappend childlist $childtree
     
     # Build the new parent tree.
-    lset xmlobj($plevel) 4 $childlist
+    lset state($plevel) 4 $childlist
 }
 
 proc tinydom::xmldecrypt {chdata} {
@@ -140,7 +141,7 @@ proc tinydom::getattribute {xmllist attrname} {
 }
 
 proc tinydom::getfirstchildwithtag {xmllist tag} {    
-    set c {}
+    set c [list]
     foreach celem [lindex $xmllist 4] {
 	if {[string equal [lindex $celem 0] $tag]} {
 	    set c $celem
