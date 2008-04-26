dbb6a6c4ff8fab91a2cd6d60a4909508e33d7fd8
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2008-04-26 05:46:26 +0000

    * Updated to 0.3.0

--- trunk/coccinella/bin/macosx/i386/tkpath/pkgIndex.tcl
+++ trunk/coccinella/bin/macosx/i386/tkpath/pkgIndex.tcl
@@ -1,5 +1,6 @@
 #
 #  We need both dynaimc lib and script support files.
 
-package ifneeded tkpath 0.2.8 "[list load [file join $dir tkpath0.2.8.dylib]]; \
+package ifneeded tkpath 0.3.0 "[list load [file join $dir tkpath0.3.0.dylib]]; \
     [list source [file join $dir tkpath.tcl]]"
+  
