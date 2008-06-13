af9c3b178b269df6794a7b15cc8a5384e49a2c38
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2008-06-13 06:44:59 +0000

    * Added Tkhtml and chasingarrows builds. Untested!

---
+++ trunk/coccinella/bin/unix/Linux/i686/chasingarrows/pkgIndex.tcl
@@ -0,0 +1,7 @@
+# pkgIndex.tcl.  Generated from pkgIndex.tcl.in by configure. 
+#
+
+package ifneeded chasingarrowselem 0.2 \
+    [list load [file join $dir chasingarrowselem[info sharedlibext]]]
+
+#*EOF* 
