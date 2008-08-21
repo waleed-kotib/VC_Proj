c2996d0ecfb55423e4893093969ebc33a001fe55
matben <matben@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2008-08-21 07:23:37 +0000

    * Just a place holder for coming ssl info UI.

---
+++ trunk/coccinella/lib/SSLInfo.tcl
@@ -0,0 +1,30 @@
+#  SSLInfo.tcl ---
+#  
+#      This file is part of The Coccinella application. 
+#      It implements UI for SSL info.
+#      
+#  Copyright (c) 2008  Mats Bengtsson
+#  
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#   
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#   
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  
+#  See the README file for license, bugs etc.
+#  
+# $Id: SSLInfo.tcl,v 1.1 2008-08-21 07:23:37 matben Exp $
+
+package provide SSLInfo 1.0
+
+namespace eval ::SSLInfo {
+
+}
+
