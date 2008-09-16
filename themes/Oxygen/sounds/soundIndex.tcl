1292769c5aeaaf71ed7d1a928a4814c0a7243315
sdevrieze <sdevrieze@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2008-09-16 06:45:18 +0000

    imported Oxygen theme

---
+++ themes/Oxygen/sounds/soundIndex.tcl
@@ -0,0 +1,10 @@
+array set sound {
+    online          online.wav
+    offline         offline.wav
+    newmsg          newmsg.wav
+    newchatmsg      newchatmsg.wav
+    newchatthread   newchatthread.wav
+    statchange      statchange.wav
+    connected       connected.wav
+    groupchatpres   clicked.wav
+}
