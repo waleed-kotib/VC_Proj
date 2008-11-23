c293d72e5bfbdf4234bc336a64a5a29af4fdb619
sdevrieze <sdevrieze@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2008-11-23 19:09:52 +0000

    compiled on OSX 10.4 to fix crash on that platform

--- trunk/coccinella/bin/macosx/i386/treectrl2.2.3/pkgIndex.tcl
+++ trunk/coccinella/bin/macosx/i386/treectrl2.2.3/pkgIndex.tcl
@@ -1,9 +1,8 @@
-if {[catch {package require Tcl 8.4}]} return
+if {[catch {package require Tcl 8.5}]} return
 set script ""
 if {![info exists ::env(TREECTRL_LIBRARY)]
     && [file exists [file join $dir treectrl.tcl]]} {
-    append script "set ::treectrl_library \"$dir\"
-"
+    append script "set ::treectrl_library \"$dir\"\n"
 }
 append script "load \"[file join $dir libtreectrl2.2.dylib]\" treectrl"
-package ifneeded treectrl 2.2.3 $script
+package ifneeded treectrl 2.2.8 $script
