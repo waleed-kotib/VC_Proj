505e954ea84b118711cf379e736e5daada41d308
sdevrieze <sdevrieze@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2009-03-14 15:52:41 +0000

    update to Tktreectrl 2.2.8

--- bin/windows/intel/treectrl/pkgIndex.tcl
+++ bin/windows/intel/treectrl/pkgIndex.tcl
@@ -1,6 +1,8 @@
-if {[catch {package require Tcl 8.4}]} return
-if {[info tclversion] == 8.4} {
-    package ifneeded treectrl 2.2.3 [list load [file join $dir treectrl22.dll] treectrl]
-} elseif {[info tclversion] == 8.5} {
-    package ifneeded treectrl 2.2.6 [list load [file join $dir 8.5 treectrl22.dll] treectrl]    
+if {[catch {package require Tcl 8.5}]} return
+set script ""
+if {![info exists ::env(TREECTRL_LIBRARY)]
+    && [file exists [file join $dir treectrl.tcl]]} {
+    append script "set ::treectrl_library \"$dir\"\n"
 }
+append script "load \"[file join $dir treectrl22.dll]\" treectrl"
+package ifneeded treectrl 2.2.8 $script
