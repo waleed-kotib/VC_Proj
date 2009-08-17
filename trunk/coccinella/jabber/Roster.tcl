4fc4a96e17cd5bb6ce5f28986d7371de6912c21b
sdevrieze <sdevrieze@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2009-08-17 06:11:46 +0000

    free for chat string and Dutch translation

--- trunk/coccinella/jabber/Roster.tcl
+++ trunk/coccinella/jabber/Roster.tcl
@@ -80,7 +80,7 @@ namespace eval ::Roster {
     array set mapShowTextToElem [list \
       [mc "Available"]       available  \
       [mc "Away"]            away       \
-      [mc "Chat"]            chat       \
+      [mc "Free For Chat"]            chat       \
       [mc "Do Not Disturb"]    dnd        \
       [mc "Extended Away"]    xa         \
       [mc "Invisible"]       invisible  \
@@ -88,7 +88,7 @@ namespace eval ::Roster {
     array set mapShowElemToText [list \
       available       [mc "Available"]     \
       away            [mc "Away"]          \
-      chat            [mc "Chat"]          \
+      chat            [mc "Free For Chat"]          \
       dnd             [mc "Do Not Disturb"]  \
       xa              [mc "Extended Away"]  \
       invisible       [mc "Invisible"]     \
