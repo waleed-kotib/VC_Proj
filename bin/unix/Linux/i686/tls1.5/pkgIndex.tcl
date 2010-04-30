0a4ca26c4bcddf57df161cf2650943099ae35d49
buzzzzdeeee <buzzzzdeeee@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2010-04-30 09:14:24 +0000

    version 1.5 -> 1.50 for tls

--- bin/unix/Linux/i686/tls1.5/pkgIndex.tcl
+++ bin/unix/Linux/i686/tls1.5/pkgIndex.tcl
@@ -5,5 +5,5 @@
 #    replace the original which didn't include the commands from "tls.tcl".
 #
 
-package ifneeded tls 1.5 "[list load [file join $dir libtls1.50.so] ] ; [list source [file join $dir tls.tcl] ]"
+package ifneeded tls 1.50 "[list load [file join $dir libtls1.50.so] ] ; [list source [file join $dir tls.tcl] ]"
 
