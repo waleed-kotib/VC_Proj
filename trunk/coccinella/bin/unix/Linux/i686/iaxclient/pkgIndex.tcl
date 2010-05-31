a1c49b6172c2b6507f89d8d1354c84062e482e62
buzzzzdeeee <buzzzzdeeee@e0b4ead7-ad73-4dbc-b1fc-b32a50b87243>
2010-05-31 07:28:09 +0000

    hopefully make the iaxclient work in the Linux binary to get the IAX Phone working.

--- trunk/coccinella/bin/unix/Linux/i686/iaxclient/pkgIndex.tcl
+++ trunk/coccinella/bin/unix/Linux/i686/iaxclient/pkgIndex.tcl
@@ -1,12 +1,15 @@
 #
 # Tcl package index file
 #
-if {[file exists /dev/dsp] && [file writable /dev/dsp]} {
-    if {![catch {open /dev/dsp "WRONLY NONBLOCK"} f]} {
-	close $f
-	package ifneeded iaxclient 0.2 \
-	  "[list load [file join $dir libiaxclient0.2.so] iaxclient]; \
-	  [list source [file join $dir iaxclient.tcl]]"
-    }
-}
+#if {[file exists /dev/dsp] && [file writable /dev/dsp]} {
+#    if {![catch {open /dev/dsp "WRONLY NONBLOCK"} f]} {
+#	close $f
+#	package ifneeded iaxclient 0.2 \
+#	  "[list load [file join $dir libiaxclient0.2.so] tcliaxclient]; \
+#	  [list source [file join $dir iaxclient.tcl]]"
+#    }
+#}
 
+package ifneeded iaxclient 0.2 \
+    "[list load [file join $dir libiaxclient0.2.so] tcliaxclient]; \
+    [list source [file join $dir iaxclient.tcl]]"
